8819e3333f22c7e1ef006da94ad27527d2e91cc5
Tommy Jung <tommy.dc.jung@gmail.com>
2020-12-29 00:24:38 -0800

    chip finish pre

---
+++ bigblade_pod/tcl/hard/gf_14/chip_finish_pre_script.tcl
@@ -0,0 +1,24 @@
+puts "BSG-info: Running script [info script]\n"
+
+
+if { $DESIGN_NAME == "bsg_chip" } {
+  route_eco
+
+  # remove the hard placement blockages created during dp.placement,
+  # so that filler cells can be inserted correctly.
+  remove_placement_blockages [get_placement_blockages tile_rp*]
+  #remove_placement_blockages [get_placement_blockages tile_right*]
+  #remove_placement_blockages [get_placement_blockages tile_left*]
+  remove_placement_blockages [get_placement_blockages north_vc_rp*]
+  remove_placement_blockages [get_placement_blockages south_vc_rp*]
+} elseif {$DESIGN_NAME == "bsg_manycore_tile_ruche"} {
+  route_eco
+} elseif {$DESIGN_NAME == "bsg_manycore_tile_vcache"} {
+  route_eco
+}
+
+
+
+puts "BSG-info: Completed script [info script]\n"
+
+
