f1304d71b08346a428c83feddd35cb9daffd305a
Tommy Jung <tommy.dc.jung@gmail.com>
2020-12-28 22:07:28 -0800

    hb clock opt cts pre script; draw wires script

---
+++ bigblade_pod/tcl/hard/gf_14/clock_opt_cts_pre_script.tcl
@@ -0,0 +1,48 @@
+puts "BSG-info: Running script [info script]\n"
+
+
+# common local proc
+proc get_y_cord {obj} {
+  set lly [get_attribute $obj bounding_box.ll_y]
+  set ury [get_attribute $obj bounding_box.ur_y]
+  return [expr ($lly+$ury)/2]
+}
+
+
+if { $DESIGN_NAME == "bsg_manycore_tile_ruche" } {
+
+  source $::env(BSG_DESIGNS_TARGET_TCL_HARD_DIR)/hb_common_variables.tcl
+  source $::env(BSG_DESIGNS_TARGET_TCL_HARD_DIR)/hb_draw_wires.tcl
+
+  set core_llx [get_attribute [get_core_area] bounding_box.ll_x]
+  set core_urx [get_attribute [get_core_area] bounding_box.ur_x]
+
+  for {set i [expr 2*$HB_RUCHE_LINK_WIDTH_P]} {$i < [expr 2*$HB_RUCHE_FACTOR_P*$HB_RUCHE_LINK_WIDTH_P]} {incr i} {
+    set net [get_nets "ruche_link_i[${i}]"]
+    set port [get_ports "ruche_link_i[${i}]"]
+    set layer [get_attribute $port layer.name]
+    set y [get_y_cord $port]
+    draw_horizontal_wire $net $layer $core_llx $core_urx $y
+  }
+
+} elseif { $DESIGN_NAME == "bsg_manycore_tile_vcache" } {
+
+  source $::env(BSG_DESIGNS_TARGET_TCL_HARD_DIR)/hb_common_variables.tcl
+  source $::env(BSG_DESIGNS_TARGET_TCL_HARD_DIR)/hb_draw_wires.tcl
+
+  set core_llx [get_attribute [get_core_area] bounding_box.ll_x]
+  set core_urx [get_attribute [get_core_area] bounding_box.ur_x]
+
+  for {set i [expr 2*$HB_WH_LINK_WIDTH_P]} {$i < [expr 2*$HB_WH_RUCHE_FACTOR_P*$HB_WH_LINK_WIDTH_P]} {incr i} {
+    set net [get_nets "wh_link_sif_i[${i}]"]
+    set port [get_ports "wh_link_sif_i[${i}]"]
+    set layer [get_attribute $port layer.name]
+    set y [get_y_cord $port]
+    draw_horizontal_wire $net $layer $core_llx $core_urx $y
+  }
+
+}
+
+
+
+puts "BSG-info: Completed script [info script]\n"
