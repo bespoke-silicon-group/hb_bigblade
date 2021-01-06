c983f0a4157538fcec9157f41900102a9078abbc
Paul Gao <gaozihou@uw.edu>
2021-01-05 21:43:57 -0800

    move pinout and iopad information to bsg_packaging and board repos

--- bigblade_io/tcl/include.tcl
+++ bigblade_io/tcl/include.tcl
@@ -10,7 +10,9 @@ set bsg_padmapping    $::env(BSG_PADMAPPING)
 set SVERILOG_INCLUDE_PATHS [join "
   $bsg_packaging_dir/common/verilog
   $bsg_packaging_dir/common/foundry/portable/verilog
-  $bsg_designs_target_dir/v
+  $bsg_packaging_dir/$bsg_package/pinouts/$bsg_pinout/common/verilog
+  $bsg_packaging_dir/$bsg_package/pinouts/$bsg_pinout/portable/verilog
+  $bsg_packaging_dir/$bsg_package/pinouts/$bsg_pinout/portable/verilog/padmappings/$bsg_padmapping
   $basejump_stl_dir/bsg_misc
   $basejump_stl_dir/bsg_cache
   $basejump_stl_dir/bsg_clk_gen
--- bigblade_io/testing/tcl/include.tcl
+++ bigblade_io/testing/tcl/include.tcl
@@ -8,9 +8,9 @@ set bsg_pinout        $::env(BSG_PINOUT)
 set bsg_padmapping    $::env(BSG_PADMAPPING)
 
 set TESTING_INCLUDE_PATHS [join "
+  $bsg_packaging_dir/$bsg_package/pinouts/$bsg_pinout/common/verilog
   $basejump_stl_dir/bsg_misc
   $basejump_stl_dir/bsg_cache
   $basejump_stl_dir/bsg_noc
   $basejump_stl_dir/bsg_tag
-  $bsg_designs_target_dir/v
 "]
