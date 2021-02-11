495c0312190e17e51acb4f92d2df1f97b5ef7e3c
tj <tommy.dc.jung@gmail.com>
2021-02-10 23:23:25 -0800

    pnr script

--- bigblade_tile/tcl/hard/gf_14/route_auto_pre_script.tcl
+++ bigblade_tile/tcl/hard/gf_14/route_auto_pre_script.tcl
@@ -1,13 +1,6 @@
 puts "BSG-info: Running script [info script]\n"
 
-
-if { $DESIGN_NAME == "bsg_chip_pod" } {
-  set ROUTE_AUTO_CREATE_SHIELDS "none"
-} elseif {$DESIGN_NAME == "bsg_manycore_tile_compute_ruche"} {
-  set ROUTE_AUTO_CREATE_SHIELDS "none"
-} elseif {$DESIGN_NAME == "bsg_manycore_tile_vcache"} {
-  set ROUTE_AUTO_CREATE_SHIELDS "none"
-}
+set ROUTE_AUTO_CREATE_SHIELDS "none"
 
 
 
--- bigblade_tile/tcl/hard/gf_14/route_opt_pre_script.tcl
+++ bigblade_tile/tcl/hard/gf_14/route_opt_pre_script.tcl
@@ -1,14 +1,7 @@
 
 puts "BSG-info: Running script [info script]\n"
 
-
-if { $DESIGN_NAME == "bsg_chip_pod" } {
-  set ROUTE_OPT_RESHIELD "none"
-} elseif {$DESIGN_NAME == "bsg_manycore_tile_compute_ruche"} {
-  set ROUTE_OPT_RESHIELD "none"
-} elseif {$DESIGN_NAME == "bsg_manycore_tile_vcache"} {
-  set ROUTE_OPT_RESHIELD "none"
-}
+set ROUTE_OPT_RESHIELD "none"
 
 
 
