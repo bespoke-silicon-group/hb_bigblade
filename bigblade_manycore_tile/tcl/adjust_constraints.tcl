a653575564b422e1511a2198f7383723572d774f
tj <tommy.dc.jung@gmail.com>
2021-02-17 17:10:30 -0800

    tweak

--- bigblade_manycore_tile/tcl/adjust_constraints.tcl
+++ bigblade_manycore_tile/tcl/adjust_constraints.tcl
@@ -85,8 +85,6 @@ proc constraint_output_ports {clk_name ports max_delay min_delay} {
   set_load -min [load_of [get_lib_pin "*/SC7P5T_INVX2_SSC14R/A"]] $ports
 }
 
-constraint_input_ports $clk_name $reset_in_port 500 0
-constraint_output_ports $clk_name $reset_out_port 500 0
 
 # ruche link delay
 set ruche_max_delay 150
@@ -152,23 +150,23 @@ set_output_delay -max 20.0 ${feedthrough_output_pins} -clock ${clk_name}
 set_load -max [load_of [get_lib_pin "*/SC7P5T_INVX8_SSC14R/A"]] $feedthrough_output_pins
 set_load -min [load_of [get_lib_pin "*/SC7P5T_INVX8_SSC14R/A"]] $feedthrough_output_pins
 
+# reset ports
+constraint_input_ports $clk_name $reset_in_port  400 40
+constraint_output_ports $clk_name $reset_out_port 400 40
 
-# multicycle paths
 set cord_in_ports [list]
 append_to_collection cord_in_ports [get_ports my_*_i*]
 append_to_collection cord_in_ports [get_ports pod_*_i*]
-
-constraint_input_ports $clk_name $cord_in_ports 500 20
-set_multicycle_path 2 -from $cord_in_ports -setup
-set_multicycle_path 1 -from $cord_in_ports -hold
+constraint_input_ports $clk_name $cord_in_ports 400 40
+#set_multicycle_path 2 -from $cord_in_ports -setup
+#set_multicycle_path 1 -from $cord_in_ports -hold
 
 set cord_out_ports [list]
 append_to_collection cord_out_ports [get_ports my_*_o*]
 append_to_collection cord_out_ports [get_ports pod_*_o*]
-
-constraint_output_ports $clk_name $cord_out_ports 500 20
-set_multicycle_path 2 -to $cord_out_ports -setup
-set_multicycle_path 1 -to $cord_out_ports -hold
+constraint_output_ports $clk_name $cord_out_ports 400 40
+#set_multicycle_path 2 -to $cord_out_ports -setup
+#set_multicycle_path 1 -to $cord_out_ports -hold
 
 
 
--- bigblade_manycore_tile/tcl/constraints.tcl
+++ bigblade_manycore_tile/tcl/constraints.tcl
@@ -90,8 +90,6 @@ proc constraint_output_ports {clk_name ports max_delay min_delay} {
   set_load -min [load_of [get_lib_pin "*/SC7P5T_INVX2_SSC14R/A"]] $ports
 }
 
-constraint_input_ports $clk_name $reset_in_port  500 0
-constraint_output_ports $clk_name $reset_out_port 500 0
 
 # FIFO input constraints
 for {set i 0} {$i < 6} {incr i} {
@@ -158,25 +156,26 @@ set_load -max [load_of [get_lib_pin "*/SC7P5T_INVX8_SSC14R/A"]] $feedthrough_out
 set_load -min [load_of [get_lib_pin "*/SC7P5T_INVX8_SSC14R/A"]] $feedthrough_output_pins
 
 
+# reset ports
+constraint_input_ports $clk_name $reset_in_port  400 40
+constraint_output_ports $clk_name $reset_out_port 400 40
 
 
-
-# multicycle paths
 set cord_in_ports [list]
 append_to_collection cord_in_ports [get_ports my_*_i*]
 append_to_collection cord_in_ports [get_ports pod_*_i*]
-
-constraint_input_ports $clk_name $cord_in_ports 500 20
-set_multicycle_path 2 -from $cord_in_ports -setup
-set_multicycle_path 1 -from $cord_in_ports -hold
+constraint_input_ports $clk_name $cord_in_ports 400 40
+#set_multicycle_path 2 -from $cord_in_ports -setup
+#set_multicycle_path 1 -from $cord_in_ports -hold
+#set_false_path -from $cord_in_ports
 
 set cord_out_ports [list]
 append_to_collection cord_out_ports [get_ports my_*_o*]
 append_to_collection cord_out_ports [get_ports pod_*_o*]
-
-constraint_output_ports $clk_name $cord_out_ports 500 20
-set_multicycle_path 2 -to $cord_out_ports -setup
-set_multicycle_path 1 -to $cord_out_ports -hold
+constraint_output_ports $clk_name $cord_out_ports 400 40
+#set_multicycle_path 2 -to $cord_out_ports -setup
+#set_multicycle_path 1 -to $cord_out_ports -hold
+#set_false_path -to $cord_out_ports
 
 
 
