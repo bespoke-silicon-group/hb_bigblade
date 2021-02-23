ad05edf7f96e8ba5efcb8f9d9789819e6a9c6a20
tj <tommy.dc.jung@gmail.com>
2021-02-22 21:01:22 -0800

    place_pins

--- bigblade_io_sdr/tcl/hard/gf_14/pin_constraints.tcl
+++ bigblade_io_sdr/tcl/hard/gf_14/pin_constraints.tcl
@@ -1,10 +1,173 @@
+puts "BSG-info: Running script [info script]\n"
+
+source -echo -verbose $::env(BSG_DESIGNS_TARGET_DIR)/../common/hb_common_variables.tcl
+
 remove_individual_pin_constraints
 remove_block_pin_constraints
+set_block_pin_constraints -allowed_layers {C4 C5 K1 K2 K3 K4}
 
-# Useful numbers for the master tile
-set tile_width  [core_width]
-set tile_height [core_height]
 
-set k_pitch 0.128
-set c_pitch 0.160
+proc place_pins_k2_k4 {pins start_x y} {
+  set curr_x $start_x
+  set i 0
 
+  while {$i < [sizeof_collection $pins]} {
+    set pin [index_collection $pins $i]
+
+    set layer ""
+    if {$i % 2 == 0} {
+      set layer "K2"
+    } else {
+      set layer "K4"
+    }
+
+    if { [bsg_pins_check_location $curr_x $y $layer] == 1 } {
+      #puts $curr_x
+      set_individual_pin_constraints -ports $pin -allowed_layers $layer -location "$curr_x $y"
+      incr i
+    }
+
+    set curr_x [expr $curr_x + 0.128]
+  }
+}
+
+proc place_pins_c5 {pins start_x y} {
+  set curr_x $start_x
+  set i 0
+
+  while {$i < [sizeof_collection $pins]} {
+    set pin [index_collection $pins $i]
+
+    set layer "C5"
+
+    if { [bsg_pins_check_location $curr_x $y $layer] == 1 } {
+      #puts $curr_x
+      set_individual_pin_constraints -ports $pin -allowed_layers $layer -location "$curr_x $y"
+      incr i
+    }
+
+    set curr_x [expr $curr_x + 0.128]
+  }
+}
+
+
+set core_llx [get_attribute [get_core_area] bounding_box.ll_x]
+set core_lly [get_attribute [get_core_area] bounding_box.ll_y]
+set core_urx [get_attribute [get_core_area] bounding_box.ur_x]
+set core_ury [get_attribute [get_core_area] bounding_box.ur_y]
+
+# ver link
+set north_input_pins [list]
+set south_input_pins [list]
+set north_output_pins [list]
+set south_output_pins [list]
+for {set i 0} {$i < $HB_LINK_WIDTH_P} {incr i} {
+  set idx [expr $i + ($HB_LINK_WIDTH_P*0)]
+  append_to_collection north_input_pins [get_ports "core_ver_link_sif_i[$idx]"]
+  set idx [expr $i + ($HB_LINK_WIDTH_P*1)]
+  append_to_collection south_output_pins [get_ports "core_ver_link_sif_o[$idx]"]
+  set idx [expr $i + ($HB_LINK_WIDTH_P*0)]
+  append_to_collection north_output_pins [get_ports "core_ver_link_sif_o[$idx]"]
+  set idx [expr $i + ($HB_LINK_WIDTH_P*1)]
+  append_to_collection south_input_pins [get_ports "core_ver_link_sif_i[$idx]"]
+}
+
+place_pins_k2_k4 $north_input_pins  [expr 0.128*8] $core_ury 
+place_pins_k2_k4 $south_output_pins [expr 0.128*8] $core_lly
+
+place_pins_c5 $north_output_pins  [expr 0.08*16] $core_ury 
+place_pins_c5 $south_input_pins   [expr 0.08*16] $core_lly
+
+
+
+
+# hor link
+source -echo -verbose $::env(BSG_DESIGNS_TARGET_DIR)/tcl/hard/gf_14/hor_pin_constraints.tcl
+
+# north misc pins
+set north_misc_pins [list]
+append_to_collection north_misc_pins [get_ports async_uplink_reset_i]
+append_to_collection north_misc_pins [get_ports async_downlink_reset_i]
+append_to_collection north_misc_pins [get_ports async_downstream_reset_i]
+append_to_collection north_misc_pins [get_ports async_token_reset_i]
+append_to_collection north_misc_pins [sort_collection [get_ports core_global_x_i] name]
+append_to_collection north_misc_pins [sort_collection [get_ports core_global_y_i] name]
+append_to_collection north_misc_pins [get_ports core_clk_i]
+append_to_collection north_misc_pins [get_ports core_reset_i]
+place_pins_k2_k4 $north_misc_pins [expr $core_urx-0.128*30] $core_ury
+
+set south_misc_pins [list]
+append_to_collection south_misc_pins [get_ports async_uplink_reset_o]
+append_to_collection south_misc_pins [get_ports async_downlink_reset_o]
+append_to_collection south_misc_pins [get_ports async_downstream_reset_o]
+append_to_collection south_misc_pins [get_ports async_token_reset_o]
+place_pins_k2_k4 $south_misc_pins [expr $core_urx-0.128*30] $core_lly
+
+
+
+proc place_pins_k1_k3 {pins start_y x} {
+  set i 0
+  set curr_y $start_y
+
+  while {$i < [sizeof_collection $pins]} {
+
+    set pin [index_collection $pins $i]
+
+    set layer ""
+    if {$i % 2 == 0} {
+      set layer "K1"
+    } else {
+      set layer "K3"
+    }
+
+    if { [bsg_pins_check_location $x $curr_y $layer] == 1} {
+      set_individual_pin_constraints -ports $pin -allowed_layers $layer -location "$x $curr_y"
+      incr i
+    }
+
+    set curr_y [expr $curr_y + 0.128]
+  }
+}
+
+# east upstream
+set east_upstream_pins [list]
+
+append_to_collection east_upstream_pins [get_ports io_rev_link_clk_o]
+for {set i 0} {$i < [sizeof_collection [get_ports io_rev_link_data_o]]} {incr i } {
+  append_to_collection east_upstream_pins [get_ports io_rev_link_data_o[$i]]
+}
+append_to_collection east_upstream_pins [get_ports io_rev_link_v_o]
+append_to_collection east_upstream_pins [get_ports io_rev_link_token_i]
+
+append_to_collection east_upstream_pins [get_ports io_fwd_link_clk_o]
+for {set i 0} {$i < [sizeof_collection [get_ports io_fwd_link_data_o]]} {incr i } {
+  append_to_collection east_upstream_pins [get_ports io_fwd_link_data_o[$i]]
+}
+append_to_collection east_upstream_pins [get_ports io_fwd_link_v_o]
+append_to_collection east_upstream_pins [get_ports io_fwd_link_token_i]
+
+place_pins_k1_k3 $east_upstream_pins $TILE_WEST_OUTPUT_OFFSET $core_llx
+
+# east downstream
+set east_downstream_pins [list]
+
+append_to_collection east_downstream_pins [get_ports io_rev_link_clk_i]
+for {set i 0} {$i < [sizeof_collection [get_ports io_rev_link_data_i]]} {incr i } {
+  append_to_collection east_downstream_pins [get_ports io_rev_link_data_i[$i]]
+}
+append_to_collection east_downstream_pins [get_ports io_rev_link_v_i]
+append_to_collection east_downstream_pins [get_ports io_rev_link_token_o]
+
+append_to_collection east_downstream_pins [get_ports io_fwd_link_clk_i]
+for {set i 0} {$i < [sizeof_collection [get_ports io_fwd_link_data_i]]} {incr i } {
+  append_to_collection east_downstream_pins [get_ports io_fwd_link_data_i[$i]]
+}
+append_to_collection east_downstream_pins [get_ports io_fwd_link_v_i]
+append_to_collection east_downstream_pins [get_ports io_fwd_link_token_o]
+
+place_pins_k1_k3 $east_downstream_pins $TILE_WEST_INPUT_OFFSET $core_llx
+
+
+
+
+puts "BSG-info: Completed script [info script]\n"
