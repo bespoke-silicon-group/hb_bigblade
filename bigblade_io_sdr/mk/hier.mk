fd756c2d4c8c9e9a050a7a1103dc59d10ed65ba9
Paul Gao <gaozihou@uw.edu>
2021-03-03 14:17:00 -0800

    update TOP_HEIR_BLOCK assignment

--- bigblade_io_sdr/mk/hier.mk
+++ bigblade_io_sdr/mk/hier.mk
@@ -1,4 +1,7 @@
-export TOP_HIER_BLOCK  :=bsg_manycore_link_ruche_to_sdr_west
-#export TOP_HIER_BLOCK  :=bsg_manycore_link_ruche_to_sdr_east
+ifeq ($(EAST_NOT_WEST),1)
+  export TOP_HIER_BLOCK := bsg_manycore_link_ruche_to_sdr_east
+else
+  export TOP_HIER_BLOCK := bsg_manycore_link_ruche_to_sdr_west
+endif
 export MID_HIER_BLOCKS :=
 export BOT_HIER_BLOCKS :=
