611d37f620a6f2e50ab62633cdb8caf218535f25
Dan Petrisko <petrisko@cs.washington.edu>
2021-03-03 09:05:13 -0800

    Updating sdr tile

--- bigblade_bp_unicore/tcl/hard/gf_14/adjust_constraints.tcl
+++ bigblade_bp_unicore/tcl/hard/gf_14/adjust_constraints.tcl
@@ -8,22 +8,13 @@ source -echo -verbose $::env(BSG_DESIGNS_TARGET_DIR)/../common/bsg_chip_misc.tcl
 # TODO: Put names in common
 set bp_clk_name "bp_clk" ;# main clock running black parrot
 set bp_clk_period_ps [get_attribute [get_clocks ${bp_clk_name}] period]
-set bp_clk_uncertainty_ps 5
-set tag_clk_name "tag_clk" ;# clock for bsg_tag
-set tag_clk_period_ps [get_attribute [get_clocks ${tag_clk_name}] period]
-set tag_clk_uncertainty_ps 5
+set bp_clk_uncertainty_ps 20
 
-remove_clocks -all
+remove_clocks [get_clocks ${bp_clk_name}]
 
 # Loosen constraint
-set loosen_factor 1.10
+set loosen_factor 1.15
 create_clock -period [expr ${loosen_factor}*${bp_clk_period_ps}] -name ${bp_clk_name} [get_ports "clk_i"]
 set_clock_uncertainty ${bp_clk_uncertainty_ps} [get_clocks ${bp_clk_name}]
-
-set loosen_factor 1.00
-# Structs are gone by this point
-set tag_clk_pins [get_ports {bsg_tag_i[3] bsg_tag_i[7] bsg_tag_i[11]}]
-create_clock -period [expr ${loosen_factor}*${tag_clk_period_ps}] -name ${tag_clk_name} ${tag_clk_pins}
-set_clock_uncertainty ${tag_clk_uncertainty_ps} [get_clocks ${tag_clk_name}]
 
 puts "BSG-info: Completed script [info script]\n"
