3d60d3585a3992b776e09fc70ff58e029641b830
Paul Gao <gaozihou@uw.edu>
2021-03-10 08:06:37 -0800

    fix bug in west module dp flow

--- bigblade_io_sdr/mk/hier.mk
+++ bigblade_io_sdr/mk/hier.mk
@@ -2,6 +2,7 @@ ifeq ($(EAST_NOT_WEST),1)
   export TOP_HIER_BLOCK := bsg_manycore_link_ruche_to_sdr_east
 else
   export TOP_HIER_BLOCK := bsg_manycore_link_ruche_to_sdr_west
+  export EAST_NOT_WEST  := 0
 endif
 export MID_HIER_BLOCKS :=
 export BOT_HIER_BLOCKS :=
