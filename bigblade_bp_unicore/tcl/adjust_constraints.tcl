caad7d77e77dd5d277fb5ec1a47a89f01aa11206
Dan Petrisko <petrisko@cs.washington.edu>
2021-03-22 06:19:56 -0700

    Updating constraints

--- bigblade_bp_unicore/tcl/adjust_constraints.tcl
+++ bigblade_bp_unicore/tcl/adjust_constraints.tcl
@@ -7,12 +7,12 @@ source -echo -verbose $::env(BSG_DESIGNS_TARGET_DIR)/../common/bsg_chip_misc.tcl
 
 # TODO: Put names in common
 # Loosen constraint
-set bp_clk_name "bp_clk" ;# main clock running black parrot
-set bp_clk_period_ps [get_attribute [get_clocks ${bp_clk_name}] period]
-set bp_clk_uncertainty_ps 20
+set core_clk_name "core_clk" ;# main clock running black parrot
+set core_clk_period_ps [get_attribute [get_clocks ${core_clk_name}] period]
+set core_clk_uncertainty_ps 20
 
 set loosen_factor 1.10
-create_clock -period [expr ${loosen_factor}*${bp_clk_period_ps}] -name ${bp_clk_name} [get_ports "clk_i"]
-set_clock_uncertainty ${bp_clk_uncertainty_ps} [get_clocks ${bp_clk_name}]
+create_clock -period [expr ${loosen_factor}*${core_clk_period_ps}] -name ${core_clk_name} [get_ports "clk_i"]
+set_clock_uncertainty ${core_clk_uncertainty_ps} [get_clocks ${core_clk_name}]
 
 puts "BSG-info: Completed script [info script]\n"
--- bigblade_bp_unicore/tcl/constraints.tcl
+++ bigblade_bp_unicore/tcl/constraints.tcl
@@ -4,9 +4,9 @@ puts "BSG-info: Running script [info script]\n"
 ## Source common scripts
 source -echo -verbose $::env(BSG_DESIGNS_TARGET_DIR)/../common/hb_design_constants.tcl
 source -echo -verbose $::env(BSG_DESIGNS_TARGET_DIR)/../common/bsg_async.constraints.tcl
-source -echo -verbose $::env(BSG_DESIGNS_TARGET_DIR)/../common/bsg_chip_sdr.constraints.tcl
 source -echo -verbose $::env(BSG_DESIGNS_TARGET_DIR)/../common/bsg_chip_cdc.constraints.tcl
 source -echo -verbose $::env(BSG_DESIGNS_TARGET_DIR)/../common/bsg_chip_misc.tcl
+source -echo -verbose $::env(BASEJUMP_STL_DIR)/hard/gf_14/bsg_link/tcl/bsg_link_sdr.constraints.tcl
 
 ########################################
 ## App Var Setup
@@ -19,15 +19,15 @@ set_app_var case_analysis_propagate_through_icg true
 
 ########################################
 ## Clock Setup
-set bp_clk_name "bp_clk" ;# main clock running black parrot
-set bp_clk_period_ps       1100
-set bp_clk_uncertainty_per 3.0
-set bp_clk_uncertainty_ps  [expr min([expr $bp_clk_period_ps*($bp_clk_uncertainty_per/100.0)], 20)]
+set core_clk_name "core_clk" ;# main clock running black parrot
+set core_clk_period_ps       1100
+set core_clk_uncertainty_per 3.0
+set core_clk_uncertainty_ps  [expr min([expr $core_clk_period_ps*($core_clk_uncertainty_per/100.0)], 20)]
 
-set bp_input_delay_min_per 2.0
-set bp_input_delay_min_ps [expr $bp_clk_period_ps*$bp_input_delay_min_per/100.0]
-set bp_input_delay_max_per 70.0
-set bp_input_delay_max_ps [expr $bp_clk_period_ps*$bp_input_delay_max_per/100.0]
+set core_input_delay_min_per 2.0
+set core_input_delay_min_ps [expr $core_clk_period_ps*$core_input_delay_min_per/100.0]
+set core_input_delay_max_per 70.0
+set core_input_delay_max_ps [expr $core_clk_period_ps*$core_input_delay_max_per/100.0]
 
 set link_clk_period_ps        1000
 set link_clk_uncertainty_per  3.0
@@ -37,25 +37,25 @@ set token_clk_period_ps       [expr 2*$link_clk_period_ps]
 set token_clk_uncertainty_per 3.0
 set token_clk_uncertainty_ps  [expr min([expr $token_clk_period_ps*($token_clk_uncertainty_per/100.0)], 20)]
 
-set max_io_output_skew_ps     200
-set max_io_input_skew_ps      200
-set io_setup_time_output      [expr ($bp_clk_period_ps/2)-$max_io_output_skew_ps]
-set io_hold_time_output       [expr 0-$max_io_output_skew_ps]
-set io_max_input_delay        [expr ($link_clk_period_ps)-$max_io_input_skew_ps]
-set io_min_input_delay        [expr $max_io_input_skew_ps]
+set max_io_output_margin_ps   200
+set max_io_input_margin_ps    200
+set io_setup_time_output      [expr ($core_clk_period_ps/2)-$max_io_output_margin_ps]
+set io_hold_time_output       [expr 0-$max_io_output_margin_ps]
+set io_max_input_delay        [expr ($link_clk_period_ps)-$max_io_input_margin_ps]
+set io_min_input_delay        [expr $max_io_input_margin_ps]
 
 ########################################
 ## Reg2Reg
-create_clock -period $bp_clk_period_ps -name $bp_clk_name [get_ports clk_i]
-set_clock_uncertainty $bp_clk_uncertainty_ps [get_clocks $bp_clk_name]
+create_clock -period $core_clk_period_ps -name $core_clk_name [get_ports clk_i]
+set_clock_uncertainty $core_clk_uncertainty_ps [get_clocks $core_clk_name]
 
 ########################################
 ## In2Reg
-set bp_input_pins [get_ports reset_i]
-append_to_collection bp_input_pins [get_ports global_y_cord_i[*]]
-append_to_collection bp_input_pins [get_ports async_*_reset_i]
-set_input_delay -min $bp_input_delay_min_ps -clock $bp_clk_name $bp_input_pins
-set_input_delay -max $bp_input_delay_max_ps -clock $bp_clk_name $bp_input_pins
+set core_input_pins [get_ports reset_i]
+append_to_collection core_input_pins [get_ports global_y_cord_i[*]]
+append_to_collection core_input_pins [get_ports async_*_reset_i]
+set_input_delay -min $core_input_delay_min_ps -clock $core_clk_name $core_input_pins
+set_input_delay -max $core_input_delay_max_ps -clock $core_clk_name $core_input_pins
 set_driving_cell -min -no_design_rule -lib_cell $LIB_CELLS(invx2) [all_inputs]
 set_driving_cell -max -no_design_rule -lib_cell $LIB_CELLS(invx8) [all_inputs]
 
@@ -64,69 +64,74 @@ set_driving_cell -no_design_rule -lib_cell "SC7P5T_CKBUFX1_SSC14R" [get_ports cl
 ########################################
 ## SDR constraints
 for {set i 0} {$i < 3} {incr i} {
+  bsg_link_sdr_constraints                                         \
+    $core_clk_name                                                 \
+    [get_ports clk_i]                                              \
+    "fwd_out_clk_$i"                                               \
+    $core_clk_period_ps                                            \
+    $max_io_output_margin_ps                                       \
+    [get_ports io_fwd_link_clk_o[$i]]                              \
+    [get_ports "io_fwd_link_data_o[$i][*] io_fwd_link_v_o[$i]"]    \
+    "fwd_in_clk_$i"                                                \
+    $link_clk_period_ps                                            \
+    $max_io_input_margin_ps                                        \
+    [get_ports io_fwd_link_clk_i[$i]]                              \
+    [get_ports "io_fwd_link_data_i[$i][*] io_fwd_link_v_i[$i]"]    \
+    "fwd_tkn_clk_$i"                                               \
+    [get_ports io_fwd_link_token_i[$i]]                            \
+    $link_clk_uncertainty_ps
 
-  # upstream (fwd)
-  set fwd_out_clk_name "fwd_out_clk_$i"
-  set fwd_tkn_clk_name "fwd_tkn_clk_$i"
-  set_load [load_of [get_lib_pin "*/SC7P5T_CKBUFX2_SSC14R/CLK"]] [get_ports io_fwd_link_clk_o[$i]]
-  constrain_output_sdr_ports [get_ports io_fwd_link_clk_o[$i]] [get_ports io_fwd_link_data_o[$i][*]] $io_setup_time_output $io_hold_time_output
-  constrain_output_sdr_ports [get_ports io_fwd_link_clk_o[$i]] [get_ports io_fwd_link_v_o[$i]] $io_setup_time_output $io_hold_time_output
-  create_clock -period $token_clk_period_ps -name $fwd_tkn_clk_name [get_ports io_fwd_link_token_i[$i]]
-  set_clock_uncertainty $token_clk_uncertainty_ps [get_clock $fwd_tkn_clk_name]
-  
-  # upstream (rev)
-  set rev_out_clk_name "rev_out_clk_$i"
-  set rev_tkn_clk_name "rev_tkn_clk_$i"
-  set_load [load_of [get_lib_pin "*/SC7P5T_CKBUFX2_SSC14R/CLK"]] [get_ports io_rev_link_clk_o[$i]]
-  constrain_output_sdr_ports [get_ports io_rev_link_clk_o[$i]] [get_ports io_rev_link_data_o[$i][*]] $io_setup_time_output $io_hold_time_output
-  constrain_output_sdr_ports [get_ports io_rev_link_clk_o[$i]] [get_ports io_rev_link_v_o[$i]] $io_setup_time_output $io_hold_time_output
-  create_clock -period $token_clk_period_ps -name $rev_tkn_clk_name [get_ports io_rev_link_token_i[$i]]
-  set_clock_uncertainty $token_clk_uncertainty_ps [get_clock $rev_tkn_clk_name]
-  
-  # downstream (fwd)
-  set fwd_in_clk_name "fwd_in_clk_$i"
-  create_clock -period $link_clk_period_ps -name $fwd_in_clk_name [get_ports io_fwd_link_clk_i[$i]]
-  set_clock_uncertainty $link_clk_uncertainty_ps [get_clock $fwd_in_clk_name]
-  set_driving_cell -no_design_rule -lib_cell "SC7P5T_CKBUFX2_SSC14R" [get_ports io_fwd_link_clk_i[$i]]
-  constrain_input_sdr_ports $fwd_in_clk_name [get_ports io_fwd_link_data_i[$i][*]] $io_max_input_delay $io_min_input_delay
-  constrain_input_sdr_ports $fwd_in_clk_name [get_ports io_fwd_link_v_i[$i]] $io_max_input_delay $io_min_input_delay
-  
-  # downstream (rev)
-  set rev_in_clk_name "rev_in_clk_$i"
-  create_clock -period $link_clk_period_ps -name $rev_in_clk_name [get_ports io_rev_link_clk_i[$i]]
-  set_clock_uncertainty $link_clk_uncertainty_ps [get_clock $rev_in_clk_name]
-  set_driving_cell -no_design_rule -lib_cell "SC7P5T_CKBUFX2_SSC14R" [get_ports io_rev_link_clk_i[$i]]
-  constrain_input_sdr_ports $rev_in_clk_name [get_ports io_rev_link_data_i[$i][*]] $io_max_input_delay $io_min_input_delay
-  constrain_input_sdr_ports $rev_in_clk_name [get_ports io_rev_link_v_i[$i]] $io_max_input_delay $io_min_input_delay
-
+  bsg_link_sdr_constraints                                         \
+    $core_clk_name                                                 \
+    [get_ports clk_i]                                              \
+    "rev_out_clk_$i"                                               \
+    $core_clk_period_ps                                            \
+    $max_io_output_margin_ps                                       \
+    [get_ports io_rev_link_clk_o[$i]]                              \
+    [get_ports "io_rev_link_data_o[$i][*] io_rev_link_v_o[$i]"]    \
+    "rev_in_clk_$i"                                                \
+    $link_clk_period_ps                                            \
+    $max_io_input_margin_ps                                        \
+    [get_ports io_rev_link_clk_i[$i]]                              \
+    [get_ports "io_rev_link_data_i[$i][*] io_rev_link_v_i[$i]"]    \
+    "rev_tkn_clk_$i"                                               \
+    [get_ports io_rev_link_token_i[$i]]                            \
+    $link_clk_uncertainty_ps
 }
 
 ########################################
-## Disabled or false paths
-bsg_chip_disable_1r1w_paths {"*regfile*rf*"}
-bsg_chip_disable_1r1w_paths {"*btb*tag_mem*"}
-
+## False paths
 set_false_path -from [get_ports global_*_cord_i]
 set_false_path -from [get_ports async_*_reset_i]
 set_false_path -to [get_ports async_*_reset_o]
 
 ########################################
+## Disable timing
+bsg_link_sdr_disable_timing_constraints
+
+########################################
+## Dont touch
+bsg_link_sdr_dont_touch_constraints [get_ports {io_*_link_data_i[*] io_*_link_v_i}]
+
+########################################
 ## CDC Paths
-set cdc_clocks [get_clocks $bp_clk_name]
-append_to_collection cdc_clocks [get_clocks fwd_in_clk_*]
-bsg_async_icl $cdc_clocks
-
-set cdc_clocks [get_clocks $bp_clk_name]
-append_to_collection cdc_clocks [get_clocks rev_in_clk_*]
-bsg_async_icl $cdc_clocks
-
-set cdc_clocks [get_clocks $bp_clk_name]
-append_to_collection cdc_clocks [get_clocks fwd_tkn_clk_*]
-bsg_async_icl $cdc_clocks
-
-set cdc_clocks [get_clocks $bp_clk_name]
-append_to_collection cdc_clocks [get_clocks rev_tkn_clk_*]
-bsg_async_icl $cdc_clocks
+for {set i 0} {$i < 3} {incr i} {
+  set cdc_clocks [get_clocks $core_clk_name]
+  append_to_collection cdc_clocks [get_clocks fwd_in_clk_${i}]
+  bsg_async_icl $cdc_clocks
+  
+  set cdc_clocks [get_clocks $core_clk_name]
+  append_to_collection cdc_clocks [get_clocks rev_in_clk_${i}]
+  bsg_async_icl $cdc_clocks
+  
+  set cdc_clocks [get_clocks $core_clk_name]
+  append_to_collection cdc_clocks [get_clocks fwd_tkn_clk_${i}]
+  bsg_async_icl $cdc_clocks
+  
+  set cdc_clocks [get_clocks $core_clk_name]
+  append_to_collection cdc_clocks [get_clocks rev_tkn_clk_${i}]
+  bsg_async_icl $cdc_clocks
+}
 
 ########################################
 ## Derate
@@ -135,13 +140,9 @@ bsg_chip_derate_mems
 #report_timing_derate
 
 ########################################
-## Dont touch
-# inputs
-set_dont_touch_network -no_propagate [get_ports io_*_link_data_i*]
-set_dont_touch_network -no_propagate [get_ports io_*_link_v_i*]
-# outputs
-set_dont_touch_network -no_propagate [get_flat_pins -filter "full_name=~*BSG_OSDR_BUF_DONT_TOUCH/Z"]
-set_dont_touch_network -no_propagate [get_flat_pins -filter "full_name=~*BSG_OSDR_CKBUF_DONT_TOUCH/Z"]
+## Disabled or false paths
+bsg_chip_disable_1r1w_paths {"*regfile*rf*"}
+bsg_chip_disable_1r1w_paths {"*btb*tag_mem*"}
 
 ########################################
 ## Ungrouping
