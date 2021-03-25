e324ca2361ad0f2f15b2c0b5f122590ddabdde72
Paul Gao <gaozihou@uw.edu>
2021-03-25 03:28:24 -0700

    preliminary placement

--- bigblade_io_link_ddr/tcl/hard/gf_14/init_design_post_script.tcl
+++ bigblade_io_link_ddr/tcl/hard/gf_14/init_design_post_script.tcl
@@ -1,5 +1,27 @@
 # Source common scripts
-source -echo -verbose $::env(BASEJUMP_STL_DIR)/hard/gf_14/bsg_link/tcl/bsg_link_sdr.constraints.tcl
+source -echo -verbose $::env(BASEJUMP_STL_DIR)/hard/gf_14/bsg_link/tcl/bsg_link_ddr.constraints.tcl
+
+proc bsg_dont_touch_regexp {arg1} {
+    set pattern "full_name=~$arg1";
+    set mycells [get_cells -hier -filter $pattern]
+    puts [concat "BSG: set dont_touch'ing " $pattern "=" [collection_to_list $mycells]]
+    set_dont_touch $mycells
+}
+
+proc bsg_dont_touch_regexp_type {arg1} {
+    set pattern "ref_name=~$arg1";
+    set mycells [get_cells -hier -filter $pattern]
+    puts [concat "BSG: set dont_touch'ing " $pattern "=" [collection_to_list $mycells]]
+    set_dont_touch $mycells
+}
+
+
+bsg_dont_touch_regexp */adt/*
+bsg_dont_touch_regexp */cdt/*
+bsg_dont_touch_regexp */fdt/*
+bsg_dont_touch_regexp *BSG_BAL41MUX*
+
+bsg_dont_touch_regexp_type *SYNC*SDFF*
 
 # set dont touch
-bsg_link_sdr_dont_touch_constraints [get_ports {io_*_link_data_i[*] io_*_link_v_i}]
+#bsg_link_sdr_dont_touch_constraints [get_ports {io_*_link_data_i[*] io_*_link_v_i}]
