ceb514dba025c36ac4f0a8a1e3c2867b2840a9ee
Paul Gao <gaozihou@uw.edu>
2021-03-25 01:59:59 -0700

    synth flow

---
+++ bigblade_io_link_ddr/tcl/parameters.tcl
@@ -0,0 +1,10 @@
+set TOPLEVEL_PARAMETERS [join "
+  ds_width_p=6
+  num_adgs_p=1
+  width_p=33
+  channel_width_p=16
+  num_channels_p=1
+  lg_fifo_depth_p=6
+  lg_credit_to_token_decimation_p=3
+  use_extra_data_bit_p=1
+"]
