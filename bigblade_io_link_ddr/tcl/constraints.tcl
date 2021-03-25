a7f4a759be7d473310381b2461ffb152b2f79cf7
Paul Gao <gaozihou@uw.edu>
2021-03-25 01:59:59 -0700

    synth flow

---
+++ bigblade_io_link_ddr/tcl/constraints.tcl
@@ -0,0 +1,123 @@
+puts "BSG-info: Running script [info script]\n"
+
+########################################
+## Source common scripts
+source -echo -verbose $::env(BSG_DESIGNS_TARGET_DIR)/../common/bsg_chip_misc.tcl
+source -echo -verbose $::env(BSG_DESIGNS_TARGET_DIR)/../common/hb_design_constants.tcl
+source -echo -verbose $::env(BSG_DESIGNS_TARGET_DIR)/../common/bsg_async.constraints.tcl
+source -echo -verbose $::env(BSG_DESIGNS_TARGET_DIR)/../common/bsg_clk_gen.constraints.tcl
+source -echo -verbose $::env(BASEJUMP_STL_DIR)/hard/gf_14/bsg_link/tcl/bsg_link_ddr.constraints.tcl
+
+
+########################################
+## App Var Setup
+# Needed for CDC multiple clock path constraints
+set_app_var timing_enable_multiple_clocks_per_reg true
+# Needed for automatic clock-gate insertions
+set_app_var case_analysis_propagate_through_icg true
+########################################
+
+
+set core_clk_name           "core_clk"
+set core_clk_period_ps      1000.0 ;# 1 GHz
+set core_clk_uncertainty_ps 20
+create_clock -period $core_clk_period_ps -name $core_clk_name [get_ports core_clk_i]
+set_clock_uncertainty $core_clk_uncertainty_ps  [get_clocks $core_clk_name]
+
+set tag_clk_name           "tag_clk"
+set tag_clk_period_ps      5000.0 ;# 200 MHz
+set tag_clk_uncertainty_ps 20
+create_clock -period $tag_clk_period_ps -name $tag_clk_name [get_ports tag_clk_i]
+set_clock_uncertainty $tag_clk_uncertainty_ps  [get_clocks $tag_clk_name]
+
+set io_clk_name           "io_clk"
+set io_clk_period_ps      1000.0 ;# 1 GHz
+set io_clk_uncertainty_ps 20
+set osc_period_ps         250.0 ;# Raw oscillator frequency
+set osc_uncertainty_ps    20
+set ds_uncertainty_ps     20
+bsg_clk_gen_clock_create clk_gen/clk_gen_inst/ ${io_clk_name} ${osc_period_ps} ${io_clk_period_ps} ${osc_uncertainty_ps} ${ds_uncertainty_ps} ${io_clk_uncertainty_ps}
+
+set ext_clk_name           "ext_clk"
+create_clock -period $osc_period_ps -name $ext_clk_name [get_ports ext_clk_i]
+set_clock_uncertainty $osc_uncertainty_ps  [get_clocks $ext_clk_name]
+
+
+# grouping ports...
+set core_valid_in_ports        [get_ports core_v_i]
+set core_data_in_ports         [get_ports core_data_i]
+set core_ready_and_out_ports   [get_ports core_ready_and_o]
+set core_valid_out_ports       [get_ports core_v_o]
+set core_data_out_ports        [get_ports core_data_o]
+set core_yumi_in_ports         [get_ports core_yumi_i]
+set tag_in_ports               [get_ports tag_*_tag_lines_i]
+
+
+proc constraint_input_ports {clk_name ports max_delay min_delay} {
+  set_input_delay -max $max_delay -clock $clk_name $ports
+  set_input_delay -min $min_delay -clock $clk_name -add_delay $ports
+  set_driving_cell -min -no_design_rule -lib_cell "SC7P5T_INVX8_SSC14R" $ports
+  set_driving_cell -max -no_design_rule -lib_cell "SC7P5T_INVX2_SSC14R" $ports
+}
+proc constraint_output_ports {clk_name ports max_delay min_delay} {
+  set_output_delay -max $max_delay -clock $clk_name $ports
+  set_output_delay -min $min_delay -clock $clk_name -add_delay $ports
+  set_load -max [load_of [get_lib_pin "*/SC7P5T_INVX8_SSC14R/A"]] $ports
+  set_load -min [load_of [get_lib_pin "*/SC7P5T_INVX2_SSC14R/A"]] $ports
+}
+
+constraint_input_ports  $core_clk_name   $core_valid_in_ports        500 0
+constraint_input_ports  $core_clk_name   $core_data_in_ports         500 0
+constraint_output_ports $core_clk_name   $core_ready_and_out_ports   500 0
+
+constraint_output_ports $core_clk_name   $core_valid_out_ports       500 0
+constraint_output_ports $core_clk_name   $core_data_out_ports        500 0
+constraint_input_ports  $core_clk_name   $core_yumi_in_ports         500 0
+
+constraint_input_ports  $core_clk_name   $tag_in_ports               500 0
+
+# false path
+set_false_path -from [get_ports async_output_disable_i]
+
+
+# Source-sync link constraints
+set link_clk_period_ps        [expr $core_clk_period_ps*2.0]
+set link_clk_uncertainty_ps   20
+set max_io_output_margin_ps   100
+set max_io_input_margin_ps    100
+
+bsg_link_ddr_constraints                    \
+  $core_clk_name                            \
+  "link_out_clk"                            \
+  $link_clk_period_ps                       \
+  $max_io_output_margin_ps                  \
+  [get_ports io_link_clk_o]                 \
+  [get_ports {io_link_data_o io_link_v_o}]  \
+  "link_in_clk"                             \
+  $link_clk_period_ps                       \
+  $max_io_input_margin_ps                   \
+  [get_ports io_link_clk_i]                 \
+  [get_ports {io_link_data_i io_link_v_i}]  \
+  "link_tkn_clk"                            \
+  [get_ports io_link_token_i]               \
+  $link_clk_uncertainty_ps
+
+# set dont touch
+#bsg_link_sdr_dont_touch_constraints [get_ports {io_*_link_data_i[*] io_*_link_v_i}]
+
+
+# CDC
+set cdc_clocks [all_clocks]
+bsg_async_icl $cdc_clocks
+
+
+# ungroup
+
+
+## Derate
+bsg_chip_derate_cells
+#bsg_chip_derate_mems
+#report_timing_derate
+
+puts "BSG-info: Completed script [info script]\n"
+
---
+++ bigblade_io_link_ddr/v/bsg_chip_io_link_ddr.v
@@ -0,0 +1,168 @@
+
+module bsg_chip_io_link_ddr
+
+ import bsg_tag_pkg::*;
+
+ #(parameter ds_width_p                      = "inv"
+  ,parameter num_adgs_p                      = "inv"
+  ,parameter width_p                         = "inv"
+  ,parameter channel_width_p                 = "inv"
+  ,parameter num_channels_p                  = "inv"
+  ,parameter lg_fifo_depth_p                 = "inv"
+  ,parameter lg_credit_to_token_decimation_p = "inv"
+  ,parameter use_extra_data_bit_p            = "inv"
+  )
+
+  (input                core_clk_i
+  ,input                ext_clk_i
+  ,input                async_output_disable_i
+
+  ,input                tag_clk_i
+  ,input  bsg_tag_s     tag_io_tag_lines_i
+  ,input  bsg_tag_s     tag_core_tag_lines_i
+  ,input  bsg_tag_s     tag_async_reset_tag_lines_i
+  ,input  bsg_tag_s     tag_osc_tag_lines_i
+  ,input  bsg_tag_s     tag_osc_trigger_tag_lines_i
+  ,input  bsg_tag_s     tag_ds_tag_lines_i
+  ,input  bsg_tag_s     tag_sel_tag_lines_i
+
+  ,input                core_v_i
+  ,input  [width_p-1:0] core_data_i
+  ,output               core_ready_and_o
+
+  ,output               core_v_o
+  ,output [width_p-1:0] core_data_o
+  ,input                core_yumi_i
+
+  ,input  [num_channels_p-1:0]                      io_link_clk_i
+  ,input  [num_channels_p-1:0]                      io_link_v_i
+  ,input  [num_channels_p-1:0][channel_width_p-1:0] io_link_data_i
+  ,output [num_channels_p-1:0]                      io_link_token_o
+
+  ,output [num_channels_p-1:0]                      io_link_clk_o
+  ,output [num_channels_p-1:0]                      io_link_v_o
+  ,output [num_channels_p-1:0][channel_width_p-1:0] io_link_data_o
+  ,input  [num_channels_p-1:0]                      io_link_token_i
+  );
+
+  wire bsg_tag_s tag_io_tag_lines_li          = {tag_clk_i, tag_io_tag_lines_i         [2:0]};
+  wire bsg_tag_s tag_core_tag_lines_li        = {tag_clk_i, tag_core_tag_lines_i       [2:0]};
+  wire bsg_tag_s tag_async_reset_tag_lines_li = {tag_clk_i, tag_async_reset_tag_lines_i[2:0]};
+  wire bsg_tag_s tag_osc_tag_lines_li         = {tag_clk_i, tag_osc_tag_lines_i        [2:0]};
+  wire bsg_tag_s tag_osc_trigger_tag_lines_li = {tag_clk_i, tag_osc_trigger_tag_lines_i[2:0]};
+  wire bsg_tag_s tag_ds_tag_lines_li          = {tag_clk_i, tag_ds_tag_lines_i         [2:0]};
+  wire bsg_tag_s tag_sel_tag_lines_li         = {tag_clk_i, tag_sel_tag_lines_i        [2:0]};
+
+  logic io_clk_lo;
+
+  bsg_chip_clk_gen
+ #(.ds_width_p             (ds_width_p)
+  ,.num_adgs_p             (num_adgs_p)
+  ) clk_gen
+  (.async_reset_tag_lines_i(tag_async_reset_tag_lines_li)
+  ,.osc_tag_lines_i        (tag_osc_tag_lines_li        )
+  ,.osc_trigger_tag_lines_i(tag_osc_trigger_tag_lines_li)
+  ,.ds_tag_lines_i         (tag_ds_tag_lines_li         )
+  ,.sel_tag_lines_i        (tag_sel_tag_lines_li        )
+  ,.async_output_disable_i (async_output_disable_i      )
+  ,.ext_clk_i              (ext_clk_i                   )
+  ,.clk_o                  (io_clk_lo                   )
+  );
+
+  typedef struct packed { 
+    logic up_link_reset;
+    logic down_link_reset;
+    logic async_token_reset;
+  } io_tag_payload_s;
+
+  typedef struct packed { 
+    logic up_link_reset;
+    logic down_link_reset;
+  } core_tag_payload_s;
+
+  io_tag_payload_s   io_tag_data_lo;
+  core_tag_payload_s core_tag_data_lo;
+
+  bsg_tag_client 
+ #(.width_p       ($bits(io_tag_data_lo))
+  ,.default_p     (0)
+  ) btc_io
+  (.bsg_tag_i     (tag_io_tag_lines_li)
+  ,.recv_clk_i    (io_clk_lo)
+  ,.recv_reset_i  (1'b0)
+  ,.recv_new_r_o  ()
+  ,.recv_data_r_o (io_tag_data_lo)
+  );
+
+  bsg_tag_client 
+ #(.width_p       ($bits(core_tag_data_lo))
+  ,.default_p     (0)
+  ) btc_core
+  (.bsg_tag_i     (tag_core_tag_lines_li)
+  ,.recv_clk_i    (core_clk_i)
+  ,.recv_reset_i  (1'b0)
+  ,.recv_new_r_o  ()
+  ,.recv_data_r_o (core_tag_data_lo)
+  );
+
+  bsg_link_ddr_upstream
+ #(.width_p                        (width_p)
+  ,.channel_width_p                (channel_width_p)
+  ,.num_channels_p                 (num_channels_p)
+  ,.lg_fifo_depth_p                (lg_fifo_depth_p)
+  ,.lg_credit_to_token_decimation_p(lg_credit_to_token_decimation_p)
+  ,.use_extra_data_bit_p           (use_extra_data_bit_p)
+  ,.use_encode_p                   (1)
+  ) uplink
+  (.core_clk_i         (core_clk_i)
+  ,.core_link_reset_i  (core_tag_data_lo.up_link_reset)
+
+  ,.core_data_i        (core_data_i)
+  ,.core_valid_i       (core_v_i)
+  ,.core_ready_o       (core_ready_and_o)
+
+  ,.io_clk_i           (io_clk_lo)
+  ,.io_link_reset_i    (io_tag_data_lo.up_link_reset)
+  ,.async_token_reset_i(io_tag_data_lo.async_token_reset)
+
+  ,.io_clk_r_o         (io_link_clk_o)
+  ,.io_data_r_o        (io_link_data_o)
+  ,.io_valid_r_o       (io_link_v_o)
+  ,.token_clk_i        (io_link_token_i)
+  );
+
+  logic [num_channels_p-1:0] downlink_reset_lo;
+  for (genvar i = 0; i < num_channels_p; i++)
+  begin: down_bss
+    bsg_sync_sync #(.width_p(1)) bss
+    (.oclk_i     (io_link_clk_i[i])
+    ,.iclk_data_i(io_tag_data_lo.down_link_reset)
+    ,.oclk_data_o(downlink_reset_lo[i])
+    );
+  end
+
+  bsg_link_ddr_downstream 
+ #(.width_p                        (width_p)
+  ,.channel_width_p                (channel_width_p)
+  ,.num_channels_p                 (num_channels_p)
+  ,.lg_fifo_depth_p                (lg_fifo_depth_p)
+  ,.lg_credit_to_token_decimation_p(lg_credit_to_token_decimation_p)
+  ,.use_extra_data_bit_p           (use_extra_data_bit_p)
+  ,.use_encode_p                   (0)
+  ) downlink
+  (.core_clk_i       (core_clk_i)
+  ,.core_link_reset_i(core_tag_data_lo.down_link_reset)
+
+  ,.io_link_reset_i  (downlink_reset_lo)
+
+  ,.core_data_o      (core_data_o)
+  ,.core_valid_o     (core_v_o)
+  ,.core_yumi_i      (core_yumi_i)
+
+  ,.io_clk_i         (io_link_clk_i)
+  ,.io_data_i        (io_link_data_i)
+  ,.io_valid_i       (io_link_v_i)
+  ,.core_token_r_o   (io_link_token_o)
+  );
+
+endmodule
