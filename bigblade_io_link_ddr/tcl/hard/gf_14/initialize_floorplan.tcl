638750bcd9400fa8067e44e0adf0cad558b21426
Paul Gao <gaozihou@uw.edu>
2021-03-30 00:08:22 -0700

    fix tcl

--- bigblade_io_link_ddr/tcl/hard/gf_14/initialize_floorplan.tcl
+++ bigblade_io_link_ddr/tcl/hard/gf_14/initialize_floorplan.tcl
@@ -7,7 +7,7 @@ initialize_floorplan \
   -control_type core \
   -coincident_boundary true \
   -shape R \
-  -side_length [list [expr $grid_width*3] [expr $grid_height*25.5]]
+  -side_length [list 40.32 96.00]
 
 
 
