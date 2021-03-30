c5cc589fb1516eb41c074f9392bb3dc1da272ced
Paul Gao <gaozihou@uw.edu>
2021-03-30 04:05:02 -0700

    hardening bsg_link_ddr

--- bigblade_io_link_ddr/tcl/constraints.tcl
+++ bigblade_io_link_ddr/tcl/constraints.tcl
@@ -103,7 +103,7 @@ bsg_link_ddr_constraints                    \
   $link_clk_uncertainty_ps
 
 # set dont touch
-#bsg_link_sdr_dont_touch_constraints [get_ports {io_*_link_data_i[*] io_*_link_v_i}]
+bsg_link_ddr_dont_touch_constraints [get_ports {io_link_data_i[*] io_link_v_i}]
 
 
 # CDC
