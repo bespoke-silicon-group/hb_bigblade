c0f0b9fee9688e1c06874cd0e6167101193d02c4
Paul Gao <gaozihou@uw.edu>
2021-03-31 16:25:33 -0700

    fix placements

--- bigblade_io_link_ddr/tcl/hard/gf_14/placement_constraints.tcl
+++ bigblade_io_link_ddr/tcl/hard/gf_14/placement_constraints.tcl
@@ -3,7 +3,7 @@ set keepout_margin_x [expr 6*[unit_width]]
 set keepout_margin_y [expr 1*[unit_height]]
 set keepout_margins [list $keepout_margin_x $keepout_margin_y $keepout_margin_x $keepout_margin_y]
 
-set harden_fifo_mem [get_cells -hier -filter "ref_name=~gf14_* && full_name=~*downstream/harden_fifo/*"]
+set harden_fifo_mem [get_cells -hier -filter "ref_name=~gf14_* && full_name=~*downstream/harden*fifo/*"]
 
 set_macro_relative_location \
   -target_object $harden_fifo_mem \
@@ -14,5 +14,10 @@ set_macro_relative_location \
 
 create_keepout_margin -type hard -outer $keepout_margins $harden_fifo_mem
 
-set clk_gen_bound [create_bound -name "clk_gen" -type soft -boundary {{28.32 64.80} {40.32 76.80}}]
+set clk_gen_bound [create_bound -name "clk_gen" -type soft -boundary {{28.32 60.00} {40.32 72.00}}]
 add_to_bound ${clk_gen_bound} [get_cells -hier -filter "full_name=~clk_gen/*"]
+
+set iddr_bound [create_bound -name "iddr" -type soft -boundary {{0.00 10.50} {16.80 17.80}}]
+set oddr_bound [create_bound -name "oddr" -type soft -boundary {{0.00 0.00} {16.80 10.50}}]
+add_to_bound ${iddr_bound} [get_cells -hier -filter "full_name=~*/iddr_phy/*"]
+add_to_bound ${oddr_bound} [get_cells -hier -filter "full_name=~*/oddr_phy/*"]
