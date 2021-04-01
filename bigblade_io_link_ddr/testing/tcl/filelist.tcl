f925d220bc5d67eebd8199105bc72af1107c8d98
Paul Gao <gaozihou@uw.edu>
2021-04-01 16:13:37 -0700

    fix post_synth and post_apr simulation

--- bigblade_io_link_ddr/testing/tcl/filelist.tcl
+++ bigblade_io_link_ddr/testing/tcl/filelist.tcl
@@ -85,6 +85,7 @@ set TESTING_SOURCE_FILES [join "
   $basejump_stl_dir/bsg_tag/bsg_tag_client.v
   $basejump_stl_dir/bsg_tag/bsg_tag_trace_replay.v
   $basejump_stl_dir/bsg_fsb/bsg_fsb_node_trace_replay.v
+  $basejump_stl_dir/bsg_misc/bsg_mux2_gatestack.v
 
   $bsg_designs_target_dir/testing/v/bsg_gateway_chip.v
   $bsg_designs_target_dir/testing/v/bsg_gateway_chip_io_link_ddr_test_node.v
