97db243e7aca811e0aa3fc733a47ce5db8d66490
Paul Gao <gaozihou@uw.edu>
2021-04-09 23:58:28 -0700

    more scripts, not working

---
+++ bigblade_hold_time_test/tcl/hard/gf_14/clock_opt_cts_pre_script.tcl
@@ -0,0 +1,7 @@
+set_app_options -name route.global.timing_driven -value false
+set_app_options -name route.track.timing_driven -value false
+set_app_options -name route.detail.timing_driven -value false
+
+set_app_options -name time.enable_si_timing_windows -value true
+set_app_options -name route.common.threshold_noise_ratio -value 0.25
+set_app_options -name route_opt.flow.xtalk_reduction -value true
