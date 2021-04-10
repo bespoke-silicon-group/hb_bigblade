39781bdd9c64ea718b72d3f79c382a89ccffc56e
Paul Gao <gaozihou@uw.edu>
2021-04-10 03:18:29 -0700

    finally fixed

--- bigblade_hold_time_test/tcl/hard/gf_14/route_opt_pre_script.tcl
+++ bigblade_hold_time_test/tcl/hard/gf_14/route_opt_pre_script.tcl
@@ -5,3 +5,5 @@ set_app_options -name route.detail.timing_driven -value false
 set_app_options -name time.enable_si_timing_windows -value true
 set_app_options -name route.common.threshold_noise_ratio -value 0.25
 set_app_options -name route_opt.flow.xtalk_reduction -value true
+
+set_scenario_status tt_0p80v_25c_nominal -leakage_power true -dynamic_power true
