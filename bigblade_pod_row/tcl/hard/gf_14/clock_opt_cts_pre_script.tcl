aa2eccbd0ef7adc7ac4188e26a5493f52838c9cd
tj <tommy.dc.jung@gmail.com>
2021-04-14 13:46:22 -0700

    save pod row clock_opt_cts_pre

--- bigblade_pod_row/tcl/hard/gf_14/clock_opt_cts_pre_script.tcl
+++ bigblade_pod_row/tcl/hard/gf_14/clock_opt_cts_pre_script.tcl
@@ -25,37 +25,43 @@ set_clock_routing_rules         \
   -net_type root                \
   -rule "bsg_cts_2w2s"          \
   -min_routing_layer "M2"       \
-  -max_routing_layer "H2"
+  -max_routing_layer "K4"
 
 set_clock_routing_rules         \
   -net_type internal            \
   -rule "bsg_cts_2w2s"          \
   -min_routing_layer "M2"       \
-  -max_routing_layer "H2"
+  -max_routing_layer "K4"
 
 set_clock_routing_rules         \
   -net_type sink                \
   -rule "bsg_cts_2w2s"          \
   -min_routing_layer "M2"       \
-  -max_routing_layer "H2"
+  -max_routing_layer "K4"
 
 
 # set balance points
 set vcache_clk_pins [get_pins "podrow/px_0__pod/*_vc_x_*__*_vc_row/clk_i"]
-set_clock_balance_points -clock manycore_clk -early -consider_for_balancing true -delay 245 -corners ffpg_0p88v_125c_sigcmin -balance_points $vcache_clk_pins
-set_clock_balance_points -clock manycore_clk -late -consider_for_balancing true -delay 271 -corners ffpg_0p88v_125c_sigcmin -balance_points $vcache_clk_pins
-set_clock_balance_points -clock manycore_clk -early -consider_for_balancing true -delay 217 -corners ffpg_0p88v_m40c_sigcmin -balance_points $vcache_clk_pins
-set_clock_balance_points -clock manycore_clk -late -consider_for_balancing true -delay 237 -corners ffpg_0p88v_m40c_sigcmin -balance_points $vcache_clk_pins
-set_clock_balance_points -clock manycore_clk -early -consider_for_balancing true -delay 433 -corners sspg_0p72v_125c_sigcmax -balance_points $vcache_clk_pins
-set_clock_balance_points -clock manycore_clk -late -consider_for_balancing true -delay 470 -corners sspg_0p72v_125c_sigcmax -balance_points $vcache_clk_pins
+#set_clock_balance_points -clock manycore_clk -early -consider_for_balancing true -delay 221 -corners ffpg_0p88v_125c_sigcmin -balance_points $vcache_clk_pins
+#set_clock_balance_points -clock manycore_clk -late -consider_for_balancing true -delay 257 -corners ffpg_0p88v_125c_sigcmin -balance_points $vcache_clk_pins
+#set_clock_balance_points -clock manycore_clk -early -consider_for_balancing true -delay 194 -corners ffpg_0p88v_m40c_sigcmin -balance_points $vcache_clk_pins
+#set_clock_balance_points -clock manycore_clk -late -consider_for_balancing true -delay 213 -corners ffpg_0p88v_m40c_sigcmin -balance_points $vcache_clk_pins
+#set_clock_balance_points -clock manycore_clk -early -consider_for_balancing true -delay 369 -corners sspg_0p72v_125c_sigcmax -balance_points $vcache_clk_pins
+#set_clock_balance_points -clock manycore_clk -late -consider_for_balancing true -delay 409 -corners sspg_0p72v_125c_sigcmax -balance_points $vcache_clk_pins
+set_clock_balance_points -clock manycore_clk -consider_for_balancing true -corners ffpg_0p88v_125c_sigcmin -balance_points $vcache_clk_pins
+set_clock_balance_points -clock manycore_clk -consider_for_balancing true -corners ffpg_0p88v_m40c_sigcmin -balance_points $vcache_clk_pins
+set_clock_balance_points -clock manycore_clk -consider_for_balancing true -corners sspg_0p72v_125c_sigcmax -balance_points $vcache_clk_pins
 
 set tile_clk_pins [get_pins "podrow/px_*__pod/mc_y_*__mc_x_*__mc/clk_i"]
-set_clock_balance_points -clock manycore_clk -early -consider_for_balancing true -delay 274 -corners ffpg_0p88v_125c_sigcmin -balance_points $tile_clk_pins
-set_clock_balance_points -clock manycore_clk -late -consider_for_balancing true -delay 290 -corners ffpg_0p88v_125c_sigcmin -balance_points $tile_clk_pins
-set_clock_balance_points -clock manycore_clk -early -consider_for_balancing true -delay 238 -corners ffpg_0p88v_m40c_sigcmin -balance_points $tile_clk_pins
-set_clock_balance_points -clock manycore_clk -late -consider_for_balancing true -delay 248 -corners ffpg_0p88v_m40c_sigcmin -balance_points $tile_clk_pins
-set_clock_balance_points -clock manycore_clk -early -consider_for_balancing true -delay 467 -corners sspg_0p72v_125c_sigcmax -balance_points $tile_clk_pins
-set_clock_balance_points -clock manycore_clk -late -consider_for_balancing true -delay 489 -corners sspg_0p72v_125c_sigcmax -balance_points $tile_clk_pins
+#set_clock_balance_points -clock manycore_clk -early -consider_for_balancing true -delay 274 -corners ffpg_0p88v_125c_sigcmin -balance_points $tile_clk_pins
+#set_clock_balance_points -clock manycore_clk -late -consider_for_balancing true -delay 290 -corners ffpg_0p88v_125c_sigcmin -balance_points $tile_clk_pins
+#set_clock_balance_points -clock manycore_clk -early -consider_for_balancing true -delay 238 -corners ffpg_0p88v_m40c_sigcmin -balance_points $tile_clk_pins
+#set_clock_balance_points -clock manycore_clk -late -consider_for_balancing true -delay 248 -corners ffpg_0p88v_m40c_sigcmin -balance_points $tile_clk_pins
+#set_clock_balance_points -clock manycore_clk -early -consider_for_balancing true -delay 467 -corners sspg_0p72v_125c_sigcmax -balance_points $tile_clk_pins
+#set_clock_balance_points -clock manycore_clk -late -consider_for_balancing true -delay 489 -corners sspg_0p72v_125c_sigcmax -balance_points $tile_clk_pins
+set_clock_balance_points -clock manycore_clk -consider_for_balancing true -corners ffpg_0p88v_125c_sigcmin -balance_points $tile_clk_pins
+set_clock_balance_points -clock manycore_clk -consider_for_balancing true -corners ffpg_0p88v_m40c_sigcmin -balance_points $tile_clk_pins
+set_clock_balance_points -clock manycore_clk -consider_for_balancing true -corners sspg_0p72v_125c_sigcmax -balance_points $tile_clk_pins
 
 # report
 report_clock_balance_points
