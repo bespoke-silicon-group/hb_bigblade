7ccf1dd6854cf74fe0612e2c187e90b533809dd1
Paul Gao <gaozihou@uw.edu>
2021-04-25 15:53:44 -0700

    timing constraint adjustments

--- bigblade_io_link_ddr/tcl/hard/gf_14/route_opt_pre_script.tcl
+++ bigblade_io_link_ddr/tcl/hard/gf_14/route_opt_pre_script.tcl
@@ -7,7 +7,7 @@
 
 #set_app_options -name time.enable_si_timing_windows -value true
 #set_app_options -name route.common.threshold_noise_ratio -value 0.25
-set_app_options -name route_opt.flow.xtalk_reduction -value true
+#set_app_options -name route_opt.flow.xtalk_reduction -value true
 #set_app_options -name route_opt.flow.enable_cto -value true
 #set_app_options -name route_opt.flow.enable_ccd -value true
 
