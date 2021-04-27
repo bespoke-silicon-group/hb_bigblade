44a9af3e6427fe44904fffb34f704a629e839c57
Paul Gao <gaozihou@uw.edu>
2021-04-26 23:55:56 -0700

    add boundary cells for macro

--- bigblade_io_link_ddr/tcl/hard/gf_14/placement_constraints.tcl
+++ bigblade_io_link_ddr/tcl/hard/gf_14/placement_constraints.tcl
@@ -1,7 +1,13 @@
 
-set keepout_margin_x [expr 6*[unit_width]]
-set keepout_margin_y [expr 1*[unit_height]]
-set keepout_margins [list $keepout_margin_x $keepout_margin_y $keepout_margin_x $keepout_margin_y]
+# before-mirror margins
+set keepout_margin_left   0.058
+set keepout_margin_bottom 0.134
+set keepout_margin_right  0.007
+set keepout_margin_top    0.134
+set keepout_margins [list $keepout_margin_left $keepout_margin_bottom $keepout_margin_right $keepout_margin_top]
+
+set boundary_cell_width  [expr 8*[unit_width]]
+set boundary_cell_height [expr 1*[unit_height]]
 
 set harden_fifo_mem [get_cells -hier -filter "ref_name=~gf14_* && full_name=~*downstream/harden*fifo/*"]
 
@@ -10,7 +16,7 @@ set_macro_relative_location \
   -target_corner tl \
   -target_orientation MY \
   -anchor_corner tl \
-  -offset [list $keepout_margin_x -$keepout_margin_y]
+  -offset [list [expr $keepout_margin_right+$boundary_cell_width] [expr -$keepout_margin_top-$boundary_cell_height]]
 
 create_keepout_margin -type hard -outer $keepout_margins $harden_fifo_mem
 
@@ -19,8 +25,8 @@ set clk_gen_io_bound  [create_bound -name "clk_gen_io"  -type soft -boundary {{2
 add_to_bound ${clk_gen_noc_bound} [get_cells -hier -filter "full_name=~clk_gen_noc/*"]
 add_to_bound ${clk_gen_io_bound}  [get_cells -hier -filter "full_name=~clk_gen_io/*"]
 
-set iddr_bound [create_bound -name "iddr" -type soft -boundary {{0.00 7.00} {20.00 20.00}}]
-set oddr_bound [create_bound -name "oddr" -type soft -boundary {{0.00 0.00} {20.00 13.00}}]
+set iddr_bound [create_bound -name "iddr" -type soft -boundary {{0.00 7.50} {15.00 20.00}}]
+set oddr_bound [create_bound -name "oddr" -type soft -boundary {{0.00 0.00} {15.00 7.50}}]
 add_to_bound ${iddr_bound} [get_cells -hier -filter "full_name=~*iddr_phy/*"]
 add_to_bound ${oddr_bound} [get_cells -hier -filter "full_name=~*oddr_phy/*"]
 
