ed2357320132634650b40e5ca95d04f35ab23235
Paul Gao <gaozihou@uw.edu>
2021-04-28 09:40:35 -0700

    initial commit of noc_mem_link module

---
+++ bigblade_noc_mem_link/tcl/hard/gf_14/init_design_post_script.tcl
@@ -0,0 +1,24 @@
+# Source common scripts
+source -echo -verbose $::env(BASEJUMP_STL_DIR)/hard/gf_14/bsg_link/tcl/bsg_link_ddr.constraints.tcl
+
+proc bsg_dont_touch_regexp {arg1} {
+    set pattern "full_name=~$arg1";
+    set mycells [get_cells -hier -filter $pattern]
+    puts [concat "BSG: set dont_touch'ing " $pattern "=" [collection_to_list $mycells]]
+    set_dont_touch $mycells
+}
+
+proc bsg_dont_touch_regexp_type {arg1} {
+    set pattern "ref_name=~$arg1";
+    set mycells [get_cells -hier -filter $pattern]
+    puts [concat "BSG: set dont_touch'ing " $pattern "=" [collection_to_list $mycells]]
+    set_dont_touch $mycells
+}
+
+
+#bsg_dont_touch_regexp */adt_DONT_TOUCH/*
+#bsg_dont_touch_regexp */cdt_DONT_TOUCH/*
+#bsg_dont_touch_regexp */fdt_DONT_TOUCH/*
+#bsg_dont_touch_regexp *BSG_BAL41MUX*
+
+bsg_dont_touch_regexp_type *SYNC*SDFF*
