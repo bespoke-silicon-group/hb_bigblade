bb025c6b530ebb20c94a402826ecefec5fcae14f
Paul Gao <gaozihou@uw.edu>
2021-04-29 00:39:15 -0700

    add round-robin arbiter, wh concentrator and sdr links into noc block

--- bigblade_noc_mem_link/v/bsg_chip_noc_mem_link.v
+++ bigblade_noc_mem_link/v/bsg_chip_noc_mem_link.v
@@ -7,17 +7,26 @@ module bsg_chip_noc_mem_link
  import bsg_noc_pkg::*;
  import bsg_tag_pkg::*;
 
- #(parameter ds_width_p                      = "inv"
-  ,parameter num_adgs_p                      = "inv"
-  ,parameter width_p                         = "inv"
-  ,parameter channel_width_p                 = "inv"
-  ,parameter num_channels_p                  = "inv"
-  ,parameter lg_fifo_depth_p                 = "inv"
-  ,parameter lg_credit_to_token_decimation_p = "inv"
-  ,parameter use_extra_data_bit_p            = "inv"
-  ,parameter tag_num_clients_p               = "inv"
-  ,parameter tag_lg_max_payload_width_p      = "inv"
-  ,parameter lg_tag_num_clients_lp           = `BSG_SAFE_CLOG2(tag_num_clients_p)
+ #(parameter ds_width_p                          = "inv"
+  ,parameter num_adgs_p                          = "inv"
+  ,parameter width_p                             = "inv"
+  ,parameter channel_width_p                     = "inv"
+  ,parameter num_channels_p                      = "inv"
+  ,parameter lg_fifo_depth_p                     = "inv"
+  ,parameter lg_credit_to_token_decimation_p     = "inv"
+  ,parameter use_extra_data_bit_p                = "inv"
+  ,parameter tag_num_clients_p                   = "inv"
+  ,parameter tag_lg_max_payload_width_p          = "inv"
+  ,parameter sdr_lg_fifo_depth_p                 = "inv"
+  ,parameter sdr_lg_credit_to_token_decimation_p = "inv"
+  ,parameter wh_ruche_factor_p                   = "inv"
+  ,parameter wh_flit_width_p                     = "inv"
+  ,parameter wh_len_width_p                      = "inv"
+  ,parameter wh_cid_width_p                      = "inv"
+  ,parameter wh_cord_width_p                     = "inv"
+  ,parameter lg_tag_num_clients_lp               = `BSG_SAFE_CLOG2(tag_num_clients_p)
+  ,parameter wh_link_sif_width_lp =
+    `bsg_ready_and_link_sif_width(wh_flit_width_p)
   )
 
   (input                              ext_io_clk_i
@@ -37,6 +46,16 @@ module bsg_chip_noc_mem_link
   ,output [1:0]                       io_link_v_o
   ,output [1:0][channel_width_p-1:0]  io_link_data_o
   ,input  [1:0]                       io_link_token_i
+
+  ,output [1:0][wh_ruche_factor_p-1:0]                      io_wh_link_clk_o
+  ,output [1:0][wh_ruche_factor_p-1:0][wh_flit_width_p-1:0] io_wh_link_data_o
+  ,output [1:0][wh_ruche_factor_p-1:0]                      io_wh_link_v_o
+  ,input  [1:0][wh_ruche_factor_p-1:0]                      io_wh_link_token_i
+
+  ,input  [1:0][wh_ruche_factor_p-1:0]                      io_wh_link_clk_i
+  ,input  [1:0][wh_ruche_factor_p-1:0][wh_flit_width_p-1:0] io_wh_link_data_i
+  ,input  [1:0][wh_ruche_factor_p-1:0]                      io_wh_link_v_i
+  ,output [1:0][wh_ruche_factor_p-1:0]                      io_wh_link_token_o
   );
 
   // ddr_tag_lines + sdr_tag_lines + noc_tag_lines
@@ -143,7 +162,96 @@ module bsg_chip_noc_mem_link
   ,.data_async_r_o(async_sdr_tag_data_lo)
   );
 
-  // TODO: temporarily loopback
-  assign core_links_li = core_links_lo;
+  // mem link round robin arbiters
+  core_link_sif_s core_links_conc_li, core_links_conc_lo;
+
+  bsg_ready_and_link_round_robin_static 
+ #(.width_p      (width_p)
+  ,.num_in_p     (2)
+  ) rr
+  (.clk_i        (noc_clk_lo)
+  ,.reset_i      (noc_reset_lo)
+  ,.single_link_i(core_links_conc_li)
+  ,.single_link_o(core_links_conc_lo)
+  ,.links_i      (core_links_lo)
+  ,.links_o      (core_links_li)
+  );
+
+  `declare_bsg_ready_and_link_sif_s(wh_flit_width_p, wh_link_sif_s);
+  wh_link_sif_s wh_link_sif_li, wh_link_sif_lo;
+
+  assign wh_link_sif_li.v             = core_links_conc_lo.v;
+  assign wh_link_sif_li.data          = core_links_conc_lo.data;
+  assign wh_link_sif_li.ready_and_rev = core_links_conc_lo.ready_and_rev;
+  assign core_links_conc_li.v             = wh_link_sif_lo.v;
+  assign core_links_conc_li.data          = wh_link_sif_lo.data;
+  assign core_links_conc_li.ready_and_rev = wh_link_sif_lo.ready_and_rev;
+
+  wh_link_sif_s [1:0][wh_ruche_factor_p-1:0] wh_unconc_link_sif_li, wh_unconc_link_sif_lo;
+  bsg_wormhole_concentrator
+ #(.flit_width_p(wh_flit_width_p)
+  ,.len_width_p (wh_len_width_p)
+  ,.cid_width_p (wh_cid_width_p)
+  ,.cord_width_p(wh_cord_width_p)
+  ,.num_in_p    (2*wh_ruche_factor_p)
+  ) conc0
+  (.clk_i  (noc_clk_lo)
+  ,.reset_i(noc_reset_lo)
+  
+  ,.links_i(wh_unconc_link_sif_lo)
+  ,.links_o(wh_unconc_link_sif_li)
+
+  ,.concentrated_link_i(wh_link_sif_li)
+  ,.concentrated_link_o(wh_link_sif_lo)
+  );
+
+  for (genvar i = 0; i < 2; i++)
+  begin: sdr
+    logic uplink_reset_sync, downstream_reset_sync;
+    bsg_sync_sync #(.width_p(1)) up_bss
+    (.oclk_i     (noc_clk_lo                        )
+    ,.iclk_data_i(async_sdr_tag_data_lo.uplink_reset)
+    ,.oclk_data_o(uplink_reset_sync                 )
+    );
+    bsg_sync_sync #(.width_p(1)) down_bss
+    (.oclk_i     (noc_clk_lo                            )
+    ,.iclk_data_i(async_sdr_tag_data_lo.downstream_reset)
+    ,.oclk_data_o(downstream_reset_sync                 )
+    );
+    for (genvar j = 0; j < wh_ruche_factor_p; j++)
+      begin: wh_sdr
+        bsg_link_sdr
+       #(.width_p                        (wh_flit_width_p)
+        ,.lg_fifo_depth_p                (sdr_lg_fifo_depth_p)
+        ,.lg_credit_to_token_decimation_p(sdr_lg_credit_to_token_decimation_p)
+        ,.bypass_upstream_twofer_fifo_p  (0)
+        ,.bypass_downstream_twofer_fifo_p(0)
+        ) sdr
+        (.core_clk_i             (noc_clk_lo)
+        ,.core_uplink_reset_i    (uplink_reset_sync)
+        ,.core_downstream_reset_i(downstream_reset_sync)
+        ,.async_downlink_reset_i (async_sdr_tag_data_lo.downlink_reset)
+        ,.async_token_reset_i    (async_sdr_tag_data_lo.token_reset)
+
+        ,.core_data_i (wh_unconc_link_sif_li[i][j].data)
+        ,.core_v_i    (wh_unconc_link_sif_li[i][j].v)
+        ,.core_ready_o(wh_unconc_link_sif_lo[i][j].ready_and_rev)
+
+        ,.core_data_o (wh_unconc_link_sif_lo[i][j].data)
+        ,.core_v_o    (wh_unconc_link_sif_lo[i][j].v)
+        ,.core_yumi_i (wh_unconc_link_sif_lo[i][j].v & wh_unconc_link_sif_li[i][j].ready_and_rev)
+
+        ,.link_clk_o  (io_wh_link_clk_o  [i][j])
+        ,.link_data_o (io_wh_link_data_o [i][j])
+        ,.link_v_o    (io_wh_link_v_o    [i][j])
+        ,.link_token_i(io_wh_link_token_i[i][j])
+
+        ,.link_clk_i  (io_wh_link_clk_i  [i][j])
+        ,.link_data_i (io_wh_link_data_i [i][j])
+        ,.link_v_i    (io_wh_link_v_i    [i][j])
+        ,.link_token_o(io_wh_link_token_o[i][j])
+        );
+      end
+  end
 
 endmodule
