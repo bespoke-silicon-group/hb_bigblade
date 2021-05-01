2f0f1ec0ec8c0fbf1e64eca9c5381a27ebf22b0e
Dan Petrisko <petrisko@cs.washington.edu>
2021-04-30 20:50:27 -0700

    Updating pin constraints

--- bigblade_brg_cgra_xcel/tcl/hard/gf_14/pin_constraints.tcl
+++ bigblade_brg_cgra_xcel/tcl/hard/gf_14/pin_constraints.tcl
@@ -4,18 +4,14 @@ puts "BSG-info: Running script [info script]\n"
 ## Source common scripts
 source -echo -verbose $::env(BSG_DESIGNS_TARGET_DIR)/../common/hb_common_variables.tcl
 
-remove_individual_pin_constraints
-remove_block_pin_constraints
-
+  remove_individual_pin_constraints
+  remove_block_pin_constraints
+  remove_placement_blockages -all
+  remove_routing_blockages -all
 
   # Useful numbers for the master tile
   set core_width [core_width]
   set core_height [core_height]
- 
-  set core_margin_t 0
-  set core_margin_b 0
-  set core_margin_r 0
-  set core_margin_l 0
  
   set core_llx [get_attribute [get_core_area] bounding_box.ll_x]
   set core_lly [get_attribute [get_core_area] bounding_box.ll_y]
@@ -24,41 +20,7 @@ remove_block_pin_constraints
   
   set k_pitch 0.128
   set c_pitch 0.160
- 
-  set array_margin 12
-  set welltab_margin 3
 
-  set reserved_width 76
-  # set reserved_height 20
-
-  # PE array
-  set pe_width  [round_up_to_nearest 47 [unit_width]]
-  set pe_height [round_up_to_nearest 85 [unit_height]]
-
-  set pe_num_x 8
-  set pe_num_y 8
-
-  set pe_margin_x 2
-  set pe_margin_y 2
-
-  set pe_origin_x [expr $core_margin_l+$welltab_margin+$reserved_width+$array_margin]
-  set pe_origin_y [expr $core_margin_b+$welltab_margin+$array_margin]
-
-  set array_width  [expr $pe_num_x*$pe_width + ($pe_num_x-1)*$pe_margin_x]
-  set array_height [expr $pe_num_y*$pe_height+ ($pe_num_y-1)*$pe_margin_y]
-
-  set sp_lx [expr $core_margin_l+$welltab_margin+1]
-  set sp_ly [expr $core_margin_b+$welltab_margin+$array_margin]
-  set sp_rx [expr $sp_lx+$reserved_width]
-  set sp_ry [expr $sp_ly+$array_margin+$array_height]
-
-  set rf_width 54
-  set rf_height 83
-  set rf_margin [expr ($sp_ry-$sp_ly)/4-$rf_height]
-
-  set total_width  [expr $core_margin_l+$core_margin_r+$welltab_margin*2+$reserved_width+$array_margin+$array_width]
-  set total_height [expr $core_margin_b+$core_margin_t+$welltab_margin*2+$array_margin+$array_height+$array_margin]
- 
   set                  misc_pins [get_ports -filter "name=~clk_i"]
   append_to_collection misc_pins [get_ports -filter "name=~reset_i"]
   append_to_collection misc_pins [get_ports -filter "name=~global_y_cord_i*"]
@@ -66,7 +28,7 @@ remove_block_pin_constraints
   
   set misc_pins_len [expr [sizeof_collection $misc_pins]]
   set start_x [expr ($core_width / 2)  - (2*$c_pitch*$misc_pins_len/2)]
-  set last_loc [bsg_pins_line_constraint $misc_pins "C5" top $start_x "self" {} 2 0]
+  set last_loc [bsg_pins_line_constraint $misc_pins "C5" top $start_x "self" {} 1 1 1]
   
   # Read horizontal pins from csv file
   set fh [open $::env(BSG_DESIGNS_TARGET_DIR)/../common/sdr_pin_y.csv]
@@ -121,22 +83,20 @@ remove_block_pin_constraints
   append_to_collection misc_pins [get_pins -hier $master_pe/* -filter "name=~is_occupied*"]
   append_to_collection misc_pins [get_pins -hier $master_pe/* -filter "name=~x_cord*"]
   append_to_collection misc_pins [get_pins -hier $master_pe/* -filter "name=~y_cord*"]
+  set misc_pins_len [sizeof_collection $misc_pins]
 
-  set num_misc_pins [expr [llength $misc_pins]]
-  set misc_spacing  [expr 0.16]
-  set start_y [expr $pe_height/2 - $misc_spacing*$num_misc_pins/2]
-  set last_loc [bsg_pins_line_constraint $misc_pins "C4" left $start_y $master_pe "" 1 1]
+  set start_y [expr ($pe_height/2) - (2*$c_pitch*$misc_pins_len/2)]
+  set last_loc [bsg_pins_line_constraint $misc_pins "C4" left $start_y $master_pe "" 1 1 1]
 
   #-------------------------------------------------------------------------
   # Cfg pins
   #-------------------------------------------------------------------------
   set pins_cfg_in [get_pins -hier $master_pe/* -filter "name=~cfg_in*"]
   set pins_cfg_out [get_pins -hier $master_pe/* -filter "name=~cfg_out*"]
-  set cfg_pins_len [expr [llength $pins_cfg_in]]
+  set cfg_pins_len [sizeof_collection $pins_cfg_in]
 
-  set cfg_spacing [expr 2*0.16]
-  set start_x [expr $pe_width/2 - $cfg_pins_len*$cfg_spacing/2]
-  set last_loc [bsg_pins_line_constraint $pins_cfg_in "C5" top $start_x $master_pe $pins_cfg_out 1 1]
+  set start_x [expr ($pe_width/2) - (2*$c_pitch*$cfg_pins_len/2)]
+  set last_loc [bsg_pins_line_constraint $pins_cfg_out "C5" top $start_x $master_pe $pins_cfg_in 1 1 1]
 
   #-------------------------------------------------------------------------
   # Msg  pins
@@ -146,38 +106,39 @@ remove_block_pin_constraints
   set msg_pin_len [expr [sizeof_collection $msg_pins_o] / 4]
 
   set msg_pins_o_N [index_collection $msg_pins_o [expr 0*$msg_pin_len] [expr 1*$msg_pin_len-1]]
-  append_to_collection msg_pins_o_N [get_pins -hier $master_pe/* -filter "name=~out__val[0]"]
-  append_to_collection msg_pins_o_N [get_pins -hier $master_pe/* -filter "name=~out__rdy[0]"]
+  append_to_collection msg_pins_o_N [get_pins -hier $master_pe/* -filter "name=~out__val[3]"]
+  append_to_collection msg_pins_o_N [get_pins -hier $master_pe/* -filter "name=~in___rdy[3]"]
   set msg_pins_i_N [index_collection $msg_pins_i [expr 0*$msg_pin_len] [expr 1*$msg_pin_len-1]]
-  append_to_collection msg_pins_i_N [get_pins -hier $master_pe/* -filter "name=~in___val[0]"]
-  append_to_collection msg_pins_i_N [get_pins -hier $master_pe/* -filter "name=~in___rdy[0]"]
+  append_to_collection msg_pins_i_N [get_pins -hier $master_pe/* -filter "name=~in___val[3]"]
+  append_to_collection msg_pins_i_N [get_pins -hier $master_pe/* -filter "name=~out__rdy[3]"]
   set msg_pins_o_S [index_collection $msg_pins_o [expr 1*$msg_pin_len] [expr 2*$msg_pin_len-1]]
-  append_to_collection msg_pins_o_S [get_pins -hier $master_pe/* -filter "name=~out__val[1]"]
-  append_to_collection msg_pins_o_S [get_pins -hier $master_pe/* -filter "name=~out__rdy[1]"]
+  append_to_collection msg_pins_o_S [get_pins -hier $master_pe/* -filter "name=~out__val[2]"]
+  append_to_collection msg_pins_o_S [get_pins -hier $master_pe/* -filter "name=~in___rdy[2]"]
   set msg_pins_i_S [index_collection $msg_pins_i [expr 1*$msg_pin_len] [expr 2*$msg_pin_len-1]]
-  append_to_collection msg_pins_i_S [get_pins -hier $master_pe/* -filter "name=~in___val[1]"]
-  append_to_collection msg_pins_i_S [get_pins -hier $master_pe/* -filter "name=~in___rdy[1]"]
+  append_to_collection msg_pins_i_S [get_pins -hier $master_pe/* -filter "name=~in___val[2]"]
+  append_to_collection msg_pins_i_S [get_pins -hier $master_pe/* -filter "name=~out__rdy[2]"]
   set msg_pins_o_W [index_collection $msg_pins_o [expr 2*$msg_pin_len] [expr 3*$msg_pin_len-1]]
-  append_to_collection msg_pins_o_W [get_pins -hier $master_pe/* -filter "name=~out__val[2]"]
-  append_to_collection msg_pins_o_W [get_pins -hier $master_pe/* -filter "name=~out__rdy[2]"]
+  append_to_collection msg_pins_o_W [get_pins -hier $master_pe/* -filter "name=~out__val[1]"]
+  append_to_collection msg_pins_o_W [get_pins -hier $master_pe/* -filter "name=~in___rdy[1]"]
   set msg_pins_i_W [index_collection $msg_pins_i [expr 2*$msg_pin_len] [expr 3*$msg_pin_len-1]]
-  append_to_collection msg_pins_i_W [get_pins -hier $master_pe/* -filter "name=~in___val[2]"]
-  append_to_collection msg_pins_i_W [get_pins -hier $master_pe/* -filter "name=~in___rdy[2]"]
+  append_to_collection msg_pins_i_W [get_pins -hier $master_pe/* -filter "name=~in___val[1]"]
+  append_to_collection msg_pins_i_W [get_pins -hier $master_pe/* -filter "name=~out__rdy[1]"]
   set msg_pins_o_E [index_collection $msg_pins_o [expr 3*$msg_pin_len] [expr 4*$msg_pin_len-1]]
-  append_to_collection msg_pins_o_E [get_pins -hier $master_pe/* -filter "name=~out__val[3]"]
-  append_to_collection msg_pins_o_E [get_pins -hier $master_pe/* -filter "name=~out__rdy[3]"]
+  append_to_collection msg_pins_o_E [get_pins -hier $master_pe/* -filter "name=~out__val[0]"]
+  append_to_collection msg_pins_o_E [get_pins -hier $master_pe/* -filter "name=~in___rdy[0]"]
   set msg_pins_i_E [index_collection $msg_pins_i [expr 3*$msg_pin_len] [expr 4*$msg_pin_len-1]]
-  append_to_collection msg_pins_i_E [get_pins -hier $master_pe/* -filter "name=~in___val[3]"]
-  append_to_collection msg_pins_i_E [get_pins -hier $master_pe/* -filter "name=~in___rdy[3]"]
+  append_to_collection msg_pins_i_E [get_pins -hier $master_pe/* -filter "name=~in___val[0]"]
+  append_to_collection msg_pins_i_E [get_pins -hier $master_pe/* -filter "name=~out__rdy[0]"]
 
-  set msg_spacing [expr 2*0.128]
-  set start_y [expr $pe_height/2 - $msg_spacing*$msg_pin_len/2]
-  set start_x [expr $pe_width/2 - $msg_spacing*$msg_pin_len/2]
+  set start_y [expr $pe_height/2 - (2*$k_pitch*$msg_pin_len/2)]
+  set start_x [expr $pe_width/2 - (1*$k_pitch*$msg_pin_len/2)]
 
-  set last_loc [bsg_pins_line_constraint $msg_pins_o_N "K2 K4" top $start_x $master_pe $msg_pins_i_S 1 1]
-  set last_loc [bsg_pins_line_constraint $msg_pins_i_N "K2 K4" top $last_loc $master_pe $msg_pins_o_S 1 1]
-  set last_loc [bsg_pins_line_constraint $msg_pins_o_E "K1 K3" right $start_y $master_pe $msg_pins_i_W 1 1]
-  set last_loc [bsg_pins_line_constraint $msg_pins_i_E "K1 K3" right $last_loc $master_pe $msg_pins_o_W 1 1]
+  set last_loc [bsg_pins_line_constraint $msg_pins_o_N "K1 K3" right  $start_y  $master_pe $msg_pins_i_S 1 1 1]
+  set last_loc [bsg_pins_line_constraint $msg_pins_i_N "K1 K3" right  $last_loc $master_pe $msg_pins_o_S 1 1 1]
+  set last_loc [bsg_pins_line_constraint $msg_pins_o_E "K2 K4" bottom $start_x  $master_pe $msg_pins_i_W 1 1 1]
+  set last_loc [bsg_pins_line_constraint $msg_pins_i_E "K2 K4" bottom $last_loc $master_pe $msg_pins_o_W 1 1 1]
+
+  remove_routing_blockages -all
 
 puts "BSG-info: Completed script [info script]\n"
 
--- bigblade_brg_cgra_xcel/tcl/hard/gf_14/placement_constraints.tcl
+++ bigblade_brg_cgra_xcel/tcl/hard/gf_14/placement_constraints.tcl
@@ -17,30 +17,6 @@ if {${DESIGN_NAME} == "brg_cgra_pod"} {
   # Floorplan variables
   #-------------------------------------------------------------------------
   
-  # Power ring metal width and spacing
-  
-  #set M1_min_width   [get_attribute [get_layer M1] min_width]
-  #set M1_min_spacing [get_attribute [get_layer M1] min_spacing]
-  
-  #set p_ring_width   [expr 48 * $M1_min_width];   # Arbitrary!
-  #set p_ring_spacing [expr 24 * $M1_min_spacing]; # Arbitrary!
-  
-  # Core bounding box margins
-  
-  #set core_margin_t [expr ([llength $pwr_net_list] * ($p_ring_width + $p_ring_spacing)) + $p_ring_spacing]
-  #set core_margin_b [expr ([llength $pwr_net_list] * ($p_ring_width + $p_ring_spacing)) + $p_ring_spacing]
-  #set core_margin_r [expr ([llength $pwr_net_list] * ($p_ring_width + $p_ring_spacing)) + $p_ring_spacing]
-  #set core_margin_l [expr ([llength $pwr_net_list] * ($p_ring_width + $p_ring_spacing)) + $p_ring_spacing]
-  set core_margin_t 0
-  set core_margin_b 0
-  set core_margin_r 0
-  set core_margin_l 0
-
-  set array_margin 12
-  set welltab_margin 3
-  set reserved_width 76  
-  # set reserved_height 20
-
   set tile_height [core_height]
   set tile_width  [core_width]
   
@@ -49,51 +25,38 @@ if {${DESIGN_NAME} == "brg_cgra_pod"} {
   set keepout_margins [list $keepout_margin_x $keepout_margin_y $keepout_margin_x $keepout_margin_y]
 
   # PE array
-  set pe_width  [round_up_to_nearest 47 [unit_width]]
-  set pe_height [round_up_to_nearest 85 [unit_height]]
+  set pe_width  [round_up_to_nearest 37 [unit_width]]
+  set pe_height [round_up_to_nearest 82 [unit_height]]
   
   set pe_num_x 8
   set pe_num_y 8
   
-  set pe_margin_x 2
-  set pe_margin_y 2
+  set pe_margin_x 3.276
+  set pe_margin_y 4.32
   
-  set pe_origin_x [expr $core_margin_l+$welltab_margin+$reserved_width+$array_margin]
-  set pe_origin_y [expr $core_margin_b+$welltab_margin+$array_margin]
+  set array_height [expr ($pe_margin_y + $pe_height) * $pe_num_y - $pe_margin_y]
+  set array_width  [expr ($pe_margin_x + $pe_width) * $pe_num_x - $pe_margin_x]
+  set pe_origin_y [expr ($tile_height - $array_height) / 2]
+  set pe_origin_x [expr $tile_width - $array_width - $keepout_margin_x]
   
-  set array_margin 12
-  set welltab_margin 3
-  
-  set array_width  [expr $pe_num_x*$pe_width + ($pe_num_x-1)*$pe_margin_x]
-  set array_height [expr $pe_num_y*$pe_height+ ($pe_num_y-1)*$pe_margin_y]
-  
-  set sp_lx [expr $core_margin_l+$welltab_margin+1]
-  set sp_ly [expr $core_margin_b+$welltab_margin+$array_margin]
-  set sp_rx [expr $sp_lx+$reserved_width]
-  set sp_ry [expr $sp_ly+$array_margin+$array_height]
+  set sp_lx [expr 5+$keepout_margin_x]
+  set sp_ly [expr $pe_origin_y + 1*$array_height/4]
+  set sp_ry [expr $pe_origin_y + 3*$array_height/4]
   
   set rf_width 54
   set rf_height 83
-  set rf_margin [expr ($sp_ry-$sp_ly)/4-$rf_height]
+  set rf_margin_y [expr ($sp_ry-$sp_ly-4*$rf_height)/3]
   
-  set total_width  [expr $core_margin_l+$core_margin_r+$welltab_margin*2+$reserved_width+$array_margin+$array_width]
-  set total_height [expr $core_margin_b+$core_margin_t+$welltab_margin*2+$array_margin+$array_height+$array_margin]
-  
-  # Take all clock ports and place them center-left
-  
-  # createFence cgra_xcel/dpath/scratchpad $sp_lx $sp_ly $sp_rx $sp_ry
- 
   set sram_mems [get_cells -hier -filter "ref_name=~gf14_* && full_name=~*brg_rf_1024_32"]
   set sram_ma [create_macro_array \
     -num_rows 4 \
     -num_cols 1 \
     -align left \
-    -horizontal_channel_height [expr ($rf_height+$rf_margin)/2+$keepout_margin_y] \
+    -horizontal_channel_height [expr $rf_margin_y] \
     -vertical_channel_width [expr $keepout_margin_x] \
     -orientation FN \
     $sram_mems
   ]
-
   create_keepout_margin -type hard -outer $keepout_margins $sram_mems
 
   set_macro_relative_location \
@@ -102,25 +65,10 @@ if {${DESIGN_NAME} == "brg_cgra_pod"} {
     -target_orientation R0 \
     -anchor_corner bl \
     -offset [list $sp_lx $sp_ly]
-   
-  #for {set idx 0} {$idx < 4} {incr idx} {
-  #  set cur_lx [expr $sp_lx]
-  #  set cur_ly [expr $sp_ly+($rf_height+$rf_margin)*$idx]
-  #  set cur_rx [expr $cur_lx+$rf_width]
-  #  set cur_ry [expr $cur_ly+$rf_height]
-  #  if {$idx > 0 && $idx < 3} {
-  #    rotate_objects \
-  #      [get_cells pod/hb_cgra_xcel/cgra_xcel/dpath/scratchpad/banks__$idx/sram/sram/brg_rf_1024_32] \
-  #      -orient FN
-  #  } else {
-  #    rotate_objects \
-  #      [get_cells pod/hb_cgra_xcel/cgra_xcel/dpath/scratchpad/banks__$idx/sram/sram/brg_rf_1024_32] \
-  #      -orient N
-  #  }
-  #  create_bound pod/hb_cgra_xcel/cgra_xcel/dpath/scratchpad/banks__$idx/sram/sram/brg_rf_1024_32 \
-  #    -name BANK_rc__$idx \
-  #    -boundary [list [list $cur_lx $cur_ly] [list $cur_rx $cur_ry]] \
-  #    -type soft
-  #  #setObjFPlanBox Instance pod/hb_cgra_xcel/cgra_xcel/dpath/scratchpad/banks__$idx/sram/sram/brg_rf_1024_32 $cur_lx $cur_ly $cur_rx $cur_ry
-  #}
+
+  set isdr_bound [create_bound -name "isdr" -type soft -boundary {{0 0} {5 757.4}}]
+  set osdr_bound [create_bound -name "osdr" -type soft -boundary {{0 0} {5 757.4}}]
+  add_to_bound ${isdr_bound} [get_cells -hier -filter "full_name=~*/isdr_phy/*"]
+  add_to_bound ${osdr_bound} [get_cells -hier -filter "full_name=~*/osdr_phy/*"]
+
 }
