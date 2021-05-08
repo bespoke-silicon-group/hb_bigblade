e8f001a716819e8cf6d6aa458f478dde44dabec3
Dan Petrisko <petrisko@cs.washington.edu>
2021-05-07 22:35:34 -0700

    Adding manual placement for buffers, moving some pins around

--- bigblade_brg_cgra_xcel/tcl/hard/gf_14/block_shaping.tcl
+++ bigblade_brg_cgra_xcel/tcl/hard/gf_14/block_shaping.tcl
@@ -25,20 +25,20 @@ set pe_margin_y 9.12
 
 set array_height [expr ($pe_margin_y + $pe_height) * $pe_num_y - $pe_margin_y]
 set array_width  [expr ($pe_margin_x + $pe_width) * $pe_num_x - $pe_margin_x]
-set pe_origin_y 7.68
+set pe_origin_y 11.68
 set pe_origin_x [expr $tile_width - $array_width - $keepout_margin_x]
 
 foreach {idx_x} {7 6 5 4 3 2 1 0} {
   foreach {idx_y} {0 1 2 3 4 5 6 7} {
     set id [expr $idx_x+$idx_y*$pe_num_x]
-    append_to_collection pe_cells [get_cells pod/hb_cgra_xcel/cgra_xcel/dpath/cgra/dpath/PE_rc__$id]
+    append_to_collection pe_cells [get_cells pod/hb_cgra_xcel/cgra_xcel/dpath/cgra_dpath_PE_rc__$id]
   }
 }
 
 bsg_create_block_array_grid $pe_cells \
   -grid mib_placement_grid \
   -relative_to core \
-  -x $pe_origin_x\
+  -x $pe_origin_x \
   -y $pe_origin_y \
   -min_channel [list $pe_margin_x $pe_margin_y] \
   -rows $pe_num_y \
