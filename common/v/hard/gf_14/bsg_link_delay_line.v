4b5b601a22963e830d1563a8a8e5095b2987a5e5
Paul Gao <gaozihou@uw.edu>
2021-05-09 03:52:24 -0700

    optimize tag clk distribution

--- common/v/hard/gf_14/bsg_link_delay_line.v
+++ common/v/hard/gf_14/bsg_link_delay_line.v
@@ -2,10 +2,12 @@
 module bsg_link_delay_line
 
  import bsg_chip_pkg::*;
+ import bsg_tag_pkg::*;
 
  #(localparam width_lp = 2+bsg_link_channel_width_gp)
 
-  (input  bsg_chip_delay_tag_lines_s tag_lines_i
+  (input  tag_clk_i
+  ,input  bsg_chip_delay_tag_lines_s tag_lines_i
   ,input  [width_lp-1:0] i
   ,output [width_lp-1:0] o
   );
@@ -24,18 +26,23 @@ module bsg_link_delay_line
     ,.S0(sel[k][0]),.S1(sel[k][1]),.Z(o[k]));
   end
 
+  wire bsg_tag_s btc0_tag_lines_li = {tag_clk_i, tag_lines_i.data[0][2:0]};
+  wire bsg_tag_s btc1_tag_lines_li = {tag_clk_i, tag_lines_i.data[1][2:0]};
+  wire bsg_tag_s btc2_tag_lines_li = {tag_clk_i, tag_lines_i.data[2][2:0]};
+  wire bsg_tag_s btc3_tag_lines_li = {tag_clk_i, tag_lines_i.clk    [2:0]};
+
   // hardcoded for bsg_link_channel_width_gp = 16
   bsg_tag_client_unsync #(.width_p(12)) btc0
-  (.bsg_tag_i     (tag_lines_i.data[0])
+  (.bsg_tag_i     (btc0_tag_lines_li)
   ,.data_async_r_o(sel[0+:6]));
   bsg_tag_client_unsync #(.width_p(12)) btc1
-  (.bsg_tag_i     (tag_lines_i.data[1])
+  (.bsg_tag_i     (btc1_tag_lines_li)
   ,.data_async_r_o(sel[6+:6]));
   bsg_tag_client_unsync #(.width_p(10)) btc2
-  (.bsg_tag_i     (tag_lines_i.data[2])
+  (.bsg_tag_i     (btc2_tag_lines_li)
   ,.data_async_r_o(sel[12+:5]));
   bsg_tag_client_unsync #(.width_p(2)) btc3
-  (.bsg_tag_i     (tag_lines_i.clk)
+  (.bsg_tag_i     (btc3_tag_lines_li)
   ,.data_async_r_o(sel[17]));
 
 endmodule
