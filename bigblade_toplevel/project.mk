186b936668fba6ffe7680d36f3ae08c8854cc0db
Dustin Richmond <dustinar@uw.edu>
2021-05-13 14:43:00 -0700

    Compile CUDA executable in a separate simulation binnary (#51)
    * Add support for compiling CUDA executable in a separate simulation binary
    * (FAST) CUDA Simulation is cuda/simv
    * (WAVE) CUDA Simulation is cuda/simv-debug
    * Update readme
    * Add overriding BSG_MACHINE_PATH and BSG_PLATFORM definitions to project.mk

--- bigblade_toplevel/project.mk
+++ bigblade_toplevel/project.mk
@@ -38,3 +38,7 @@ BASEJUMP_STL_DIR       := $(BLADERUNNER_ROOT)/basejump_stl
 BASEJUMP_STL_COMMIT_ID := $(shell cd $(BASEJUMP_STL_DIR); git rev-parse --short HEAD)
 
 FPGA_IMAGE_VERSION     := 6.0.0
+BSG_MACHINE_PATH       := $(BLADERUNNER_ROOT)/testing/rtl/out
+$(warning BSG MAKE WARN: Overriding BSG_MACHINE_PATH with $(BSG_MACHINE_PATH))
+BSG_PLATFORM           := tapeout-vcs
+$(warning BSG MAKE WARN: Overriding BSG_PLATFORM with $(BSG_PLATFORM))
