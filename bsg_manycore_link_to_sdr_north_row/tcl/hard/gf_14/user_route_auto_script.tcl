7287de4d979a158d20b1b5eea05ebd880dedd312
tj <tommy.dc.jung@gmail.com>
2021-05-14 17:52:45 -0700

    sdr row apr

---
+++ bsg_manycore_link_to_sdr_north_row/tcl/hard/gf_14/user_route_auto_script.tcl
@@ -0,0 +1,13 @@
+set all_nets [list]
+
+set cells [get_cells "sdr_x_*__sdr_n"]
+append_to_collection all_nets [get_nets -of [get_pins -of $cells -filter "name=~core_reset*"]]
+append_to_collection all_nets [get_nets -of [get_pins -of $cells -filter "name=~core_global*"]]
+append_to_collection all_nets [get_nets -of [get_pins -of $cells -filter "name=~core_link_sif*"]]
+append_to_collection all_nets [get_nets -of [get_pins -of $cells -filter "name=~async*"]]
+append_to_collection all_nets [get_nets -of [get_pins -of $cells -filter "name=~io*"]]
+
+route_group -nets $all_nets
+
+set_attribute -objects $all_nets -name physical_status -value locked
+
