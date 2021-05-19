fca0194184d5e2e47f8a53ccdc3b0103ff1bac17
Scott Davidson <scottdavidson1992@gmail.com>
2021-05-19 01:02:56 -0700

    Adding thi and tlo nets with hardened bsg_tiehi and bsg_tielo cells

--- toplevel_block/tcl/filelist.tcl
+++ toplevel_block/tcl/filelist.tcl
@@ -5,6 +5,7 @@
 # definitions may be needed by the subsequent files
 #-------------------------------------------------------------------------------
 
+set basejump_stl_dir        $::env(BASEJUMP_STL_DIR)
 set bsg_designs_target_dir  $::env(BSG_DESIGNS_TARGET_DIR)
 set bsg_packaging_dir       $::env(BSG_PACKAGING_DIR)
 set bsg_package             $::env(BSG_PACKAGE)
@@ -12,9 +13,12 @@ set bsg_pinout              $::env(BSG_PINOUT)
 set bsg_padmapping          $::env(BSG_PADMAPPING)
 
 set SVERILOG_SOURCE_FILES [join "
+  $basejump_stl_dir/bsg_misc/bsg_tielo.v
+  $basejump_stl_dir/bsg_misc/bsg_tiehi.v
   $bsg_designs_target_dir/v/bsg_chip_block.v
 "]
 
 set SVERILOG_INCLUDE_PATHS [join "
+  $basejump_stl_dir/bsg_misc
 "]
 
--- toplevel_block/tcl/hard/gf_14/filelist_deltas.tcl
+++ toplevel_block/tcl/hard/gf_14/filelist_deltas.tcl
@@ -5,6 +5,7 @@
 # definitions may be needed by the subsequent files
 #-------------------------------------------------------------------------------
 
+set basejump_stl_dir        $::env(BASEJUMP_STL_DIR)
 set bsg_designs_target_dir  $::env(BSG_DESIGNS_TARGET_DIR)
 set bsg_packaging_dir       $::env(BSG_PACKAGING_DIR)
 set bsg_package             $::env(BSG_PACKAGE)
@@ -12,6 +13,8 @@ set bsg_pinout              $::env(BSG_PINOUT)
 set bsg_padmapping          $::env(BSG_PADMAPPING)
 
 set HARD_SWAP_FILELIST [join "
+  $basejump_stl_dir/hard/gf_14/bsg_misc/bsg_tielo.v
+  $basejump_stl_dir/hard/gf_14/bsg_misc/bsg_tiehi.v
 "]
 
 set NETLIST_SOURCE_FILES [join "
