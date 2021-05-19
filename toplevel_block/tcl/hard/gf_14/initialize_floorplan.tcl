a56840bf721d5c81ac2ebcf1717007daa84a50f6
Scott Davidson <scottdavidson1992@gmail.com>
2021-05-19 01:51:13 -0700

    Update to physical constraints

--- toplevel_block/tcl/hard/gf_14/initialize_floorplan.tcl
+++ toplevel_block/tcl/hard/gf_14/initialize_floorplan.tcl
@@ -1,1 +1,8 @@
-initialize_floorplan -control_type core -coincident_boundary true -shape R -side_length {10526.04 8526.24} -core_offset {0 0}
+
+initialize_floorplan \
+  -control_type core \
+  -coincident_boundary true \
+  -shape R \
+  -side_length {10665.984 8665.92} \
+  -core_offset {1 1}
+
