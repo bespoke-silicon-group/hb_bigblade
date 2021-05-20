429d7556f8b66a812e6973e9b241daa21082c196
Paul Gao <gaozihou@uw.edu>
2021-05-20 02:56:05 -0700

    enable starrc before route_opt

--- bigblade_toplevel_block_ddr_sdr_misc/tcl/hard/gf_14/user_route_opt_script.tcl
+++ bigblade_toplevel_block_ddr_sdr_misc/tcl/hard/gf_14/user_route_opt_script.tcl
@@ -33,6 +33,53 @@ proc insert_buffers_on_propagated_nets {port cell num prefix buffer} {
 }
 
 
+# Speed up wires, runtime long but significantly improve timing
+add_redundant_vias
+
+# Fix DRCs
+route_eco
+
+
+# Enable StarRC extraction before route optimization
+if {[file exists [which $ROUTE_OPT_STARRC_CONFIG_FILE]]} {
+	set_app_options -name extract.starrc_mode -value true
+	set_starrc_in_design -config $ROUTE_OPT_STARRC_CONFIG_FILE ;# example: route_opt.starrc_config_example.txt
+	
+	## Update timing with StarRC extraction
+	;#
+	;# The tool automatically performs RC extraction when it updates the timing information.
+	;#
+	;# Timing is automatically updated by commands that need the information, such as the
+	;# report_timing command. The update_timing command explicitly prepares the design for
+	;# further analysis. The default is to perform an incremental analysis, which updates
+	;# only out-of-date information and runs more quickly. Since there is no change in design
+	;# between first and second route_opt runs, automatic incremental analysis means no
+	;# analysis at all. Therefore, second route_opt run will use out-of-date timing and
+	;# RC extraction, resulting in poor QoR.
+	;#
+	;# To use the parasitic information from the StarRC extraction, we override this default
+	;# behavior using the -full option, which causes the entire timing update to be performed
+	;# from the beginning. Runtime is longer with -full option.
+	puts "RM-info: Running update_timing with StarRC extraction"
+	update_timing -full
+} elseif {$ROUTE_OPT_STARRC_CONFIG_FILE != ""} {
+	puts "RM-error: ROUTE_OPT_STARRC_CONFIG_FILE($ROUTE_OPT_STARRC_CONFIG_FILE) is invalid. Please correct it."
+}
+
+
+# Report Timing
+set pre_in_setup  [get_attribute -name slack -object [get_timing_paths -group REGIN  -delay_type max]]
+set pre_in_hold   [get_attribute -name slack -object [get_timing_paths -group REGIN  -delay_type min]]
+set pre_out_setup [get_attribute -name slack -object [get_timing_paths -group REGOUT -delay_type max]]
+set pre_out_hold  [get_attribute -name slack -object [get_timing_paths -group REGOUT -delay_type min]]
+puts "BSG-info: REGIN  group SETUP pre route_opt slack: ${pre_in_setup}"
+puts "BSG-info: REGIN  group HOLD  pre route_opt slack: ${pre_in_hold}"
+puts "BSG-info: REGOUT group SETUP pre route_opt slack: ${pre_out_setup}"
+puts "BSG-info: REGOUT group HOLD  pre route_opt slack: ${pre_out_hold}"
+
+
+
+
 # Route optimization
 set opt_buffer "SC7P5T_CKBUFX8_SSC14R"
 set opt_prefix "bsg_opt"
@@ -132,34 +179,3 @@ puts "BSG-info: REGOUT group SETUP post route_opt slack: ${post_out_setup}"
 puts "BSG-info: REGOUT group HOLD  post route_opt slack: ${post_out_hold}"
 
 
-# Speed up wires, runtime long but significantly improve timing
-add_redundant_vias
-
-# Fix DRCs
-route_eco
-
-# StarRC extraction
-if {[file exists [which $ROUTE_OPT_STARRC_CONFIG_FILE]]} {
-	set_app_options -name extract.starrc_mode -value true
-	set_starrc_in_design -config $ROUTE_OPT_STARRC_CONFIG_FILE ;# example: route_opt.starrc_config_example.txt
-	
-	## Update timing with StarRC extraction
-	;#
-	;# The tool automatically performs RC extraction when it updates the timing information.
-	;#
-	;# Timing is automatically updated by commands that need the information, such as the
-	;# report_timing command. The update_timing command explicitly prepares the design for
-	;# further analysis. The default is to perform an incremental analysis, which updates
-	;# only out-of-date information and runs more quickly. Since there is no change in design
-	;# between first and second route_opt runs, automatic incremental analysis means no
-	;# analysis at all. Therefore, second route_opt run will use out-of-date timing and
-	;# RC extraction, resulting in poor QoR.
-	;#
-	;# To use the parasitic information from the StarRC extraction, we override this default
-	;# behavior using the -full option, which causes the entire timing update to be performed
-	;# from the beginning. Runtime is longer with -full option.
-	puts "RM-info: Running update_timing with StarRC extraction"
-	update_timing -full
-} elseif {$ROUTE_OPT_STARRC_CONFIG_FILE != ""} {
-	puts "RM-error: ROUTE_OPT_STARRC_CONFIG_FILE($ROUTE_OPT_STARRC_CONFIG_FILE) is invalid. Please correct it."
-}
