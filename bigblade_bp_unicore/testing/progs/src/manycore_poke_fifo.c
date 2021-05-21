8d6b299c60ce61e0302a48cd617ff568c97562b1
sripathi-muralitharan <sripathimuralitharan@gmail.com>
2021-05-21 14:38:51 -0700

    Some QoL improvements

--- bigblade_bp_unicore/testing/progs/src/manycore_poke_fifo.c
+++ bigblade_bp_unicore/testing/progs/src/manycore_poke_fifo.c
@@ -1,53 +1,9 @@
-
 #include <stdint.h>
-#include "bp_utils.h"
-
-typedef struct response_packet {
-        uint8_t   x_dst; //!< x coordinate of the requester
-        uint8_t   y_dst; //!< y coordinate of the requester
-        uint8_t   load_id; //!< read response id
-        uint32_t  data; //!< packet's payload data
-        uint8_t   op;    //!< opcode
-        uint8_t   reserved[8];
-} __attribute__((packed)) hb_mc_response_packet_t;
-
-typedef struct request_packet {
-        uint8_t  x_dst; //!< x coordinate of the responder
-        uint8_t  y_dst; //!< y coordinate of the responder
-        uint8_t  x_src; //!< x coordinate of the requester
-        uint8_t  y_src; //!< y coordinate of the requester
-        uint32_t data;  //!< packet's payload data
-        uint8_t  reg_id; //!< 5-bit id for load or amo
-        uint8_t  op_v2;  //!< 4-bit byte mask
-        uint32_t addr;  //!< address field (EPA)
-        uint8_t  reserved[2];
-}  __attribute__((packed)) hb_mc_request_packet_t;
-
-typedef union packet {
-        hb_mc_request_packet_t request; /**/
-        hb_mc_response_packet_t response; /* from the Hammerblade Manycore */
-        uint32_t words[4];
-} hb_mc_packet_t;
+#include <bp_utils.h>
 
 void main(uint64_t argc, char * argv[]) {
-    
-    uint64_t bp_cfg_offset = 0x200000;
-    volatile uint64_t *did_mask_addr = (uint64_t *) (0x0009 + bp_cfg_offset);
-    volatile uint64_t *sac_mask_addr = (uint64_t *) (0x000a + bp_cfg_offset);
-
     // All addresses >= 0x80000000 live in the manycore. Therefore, enable all domains
     *did_mask_addr = 0xFFF;
-    
-    uint64_t  mc_host_bit = 0b111000000000000000000000000000000000000000;
-
-    volatile uint64_t *mc_link_bp_req_fifo_addr     = (uint64_t *) (0x01000 | mc_host_bit );
-    volatile uint64_t *mc_link_bp_req_credits_addr  = (uint64_t *) (0x02000 | mc_host_bit );
-    volatile uint64_t *mc_link_bp_resp_fifo_addr    = (uint64_t *) (0x03000 | mc_host_bit );
-    volatile uint64_t *mc_link_bp_resp_entries_addr = (uint64_t *) (0x04000 | mc_host_bit );
-    volatile uint64_t *mc_link_mc_req_fifo_addr     = (uint64_t *) (0x05000 | mc_host_bit );
-    volatile uint64_t *mc_link_mc_req_entries_addr  = (uint64_t *) (0x06000 | mc_host_bit );
-    volatile uint64_t *mc_link_rom_start_addr       = (uint64_t *) (0x07000 | mc_host_bit );
-    volatile uint64_t *mc_link_rom_end_addr         = (uint64_t *) (0x07fff | mc_host_bit );
 
     // Set up EPA mapping according to vanilla core map
     // For now, store to some manycore V$ EVA
