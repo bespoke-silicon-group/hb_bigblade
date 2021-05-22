7b755d71cf7e400beeac60a13a98c81b551540ac
Paul Gao <gaozihou@uw.edu>
2021-05-22 12:16:07 -0700

    fix

--- bigblade_toplevel/testing/v/bsg_gateway_chip.v
+++ bigblade_toplevel/testing/v/bsg_gateway_chip.v
@@ -30,12 +30,9 @@ module bsg_gateway_chip
   assign p_pad_CT0_v_o = p_async_output_disable_lo;
   assign p_pad_CT0_0_o = p_io_clk_lo;
   assign p_pad_CT0_1_o = p_noc_clk_lo;
-  assign p_pad_CT0_2_o = p_mc_clk_lo;
-  assign p_pad_CT0_3_o = p_mc_clk_lo;
-  assign p_pad_CT0_4_o = p_mc_clk_lo;
-  assign p_pad_CT0_5_o = p_mc_clk_lo;
+  assign {p_pad_CT0_5_o, p_pad_CT0_4_o, p_pad_CT0_3_o, p_pad_CT0_2_o} = {(hb_num_pods_y_gp){p_mc_clk_lo}};
 
-  wire [3:0] p_mc_clk_monitor_li = {p_pad_CT0_3_i, p_pad_CT0_2_i, p_pad_CT0_1_i, p_pad_CT0_0_i};
+  wire [hb_num_pods_y_gp-1:0] p_mc_clk_monitor_li = {p_pad_CT0_3_i, p_pad_CT0_2_i, p_pad_CT0_1_i, p_pad_CT0_0_i};
 
 
   //////////////////////////////////////////////////
