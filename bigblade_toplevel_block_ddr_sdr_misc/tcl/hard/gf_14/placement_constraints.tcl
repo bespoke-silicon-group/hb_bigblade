f0c28c29d70dd013a0b1c7c3ed43e433ab473a25
Paul Gao <gaozihou@uw.edu>
2021-05-22 23:49:29 -0700

    fix placement constraints

--- bigblade_toplevel_block_ddr_sdr_misc/tcl/hard/gf_14/placement_constraints.tcl
+++ bigblade_toplevel_block_ddr_sdr_misc/tcl/hard/gf_14/placement_constraints.tcl
@@ -27,29 +27,32 @@ set pod_gap    134.72
 set noc_mem_width  107.52
 set noc_mem_height 155.52
 
-set hor_gap [expr ($core_urx-$bp_width-$cgra_width-$pod_width)/2]
-set ver_gap [expr ($core_ury-4*$pod_height-3*$pod_gap)/2]
+set core_width  [expr $core_urx-$core_llx]
+set core_height [expr $core_ury-$core_lly]
+
+set hor_gap [expr ($core_width-$bp_width-$cgra_width-$pod_width)/2]
+set ver_gap [expr ($core_height-4*$pod_height-3*$pod_gap)/2]
 
 
 
 # pod blockages
-set blockage_dim "{{[expr $hor_gap+$bp_width] $ver_gap} {[expr $core_urx-$hor_gap-$cgra_width] [expr $core_ury-$ver_gap]}}"
+set blockage_dim "{{[expr $core_llx+$hor_gap+$bp_width] [expr $core_lly+$ver_gap]} {[expr $core_urx-$hor_gap-$cgra_width] [expr $core_ury-$ver_gap]}}"
 create_placement_blockage -boundary $blockage_dim
 create_routing_blockage -layers [get_layers] -boundary $blockage_dim
 
 # bp blockages
 for {set i 0} {$i < 4} {incr i} {
-  set lly [expr $ver_gap+$i*($pod_height+$pod_gap)+$vcache_height]
-  set ury [expr $ver_gap+$i*($pod_height+$pod_gap)+$pod_height-$vcache_height-$tile_height]
-  set blockage_dim "{{$hor_gap $lly} {[expr $hor_gap+$bp_width] $ury}}"
+  set lly [expr $core_lly+$ver_gap+$i*($pod_height+$pod_gap)+$vcache_height]
+  set ury [expr $core_lly+$ver_gap+$i*($pod_height+$pod_gap)+$pod_height-$vcache_height-$tile_height]
+  set blockage_dim "{{[expr $core_llx+$hor_gap] $lly} {[expr $core_llx+$hor_gap+$bp_width] $ury}}"
   create_placement_blockage -boundary $blockage_dim
   create_routing_blockage -layers [get_layers] -boundary $blockage_dim
 }
 
 # cgra blockages
 for {set i 0} {$i < 4} {incr i} {
-  set lly [expr $ver_gap+$i*($pod_height+$pod_gap)+$vcache_height]
-  set ury [expr $ver_gap+$i*($pod_height+$pod_gap)+$pod_height-$vcache_height]
+  set lly [expr $core_lly+$ver_gap+$i*($pod_height+$pod_gap)+$vcache_height]
+  set ury [expr $core_lly+$ver_gap+$i*($pod_height+$pod_gap)+$pod_height-$vcache_height]
   set blockage_dim "{{[expr $core_urx-$hor_gap-$cgra_width] $lly} {[expr $core_urx-$hor_gap] $ury}}"
   create_placement_blockage -boundary $blockage_dim
   create_routing_blockage -layers [get_layers] -boundary $blockage_dim
@@ -60,35 +63,37 @@ for {set i 0} {$i < 4} {incr i} {
 # tag_cell placement
 set                TAG_cell [get_cells -of_object [get_nets -of_object [get_ports "pad_ML0_1_i_int"]]]
 set_attribute     $TAG_cell orientation R0
-move_object       $TAG_cell -x [round_down_to_nearest 1404.742 [unit_width]] -y [round_down_to_nearest [expr $core_ury-2*[unit_height]] [unit_height]]
+move_object       $TAG_cell \
+    -x [expr $core_llx+[round_down_to_nearest 1404.742 [unit_width]]] \
+    -y [expr $core_lly+[round_down_to_nearest [expr $core_height-2*[unit_height]] [unit_height]]]
 set_fixed_objects $TAG_cell
 
 
 
 # noc_mem placement
-set noc_mem_west_x [round_up_to_nearest $hor_gap $grid_width]
-set noc_mem_east_x [expr [round_down_to_nearest [expr $core_urx-$hor_gap] $grid_width]-0.5*$grid_width]
+set noc_mem_west_x [expr $core_llx+[round_up_to_nearest $hor_gap $grid_width]]
+set noc_mem_east_x [expr $core_llx+[round_down_to_nearest [expr $core_width-$hor_gap] $grid_width]-0.5*$grid_width]
 
 for {set i 0} {$i < 2} {incr i} {
 
   set offset_x [expr {$i == 0} ? {$noc_mem_west_x} : {$noc_mem_east_x}]
   set ori [expr {$i == 0} ? {"R0"} : {"MY"}]
   set                mem_cell [get_cells "mem_link_[expr 0+$i*4]__link"]
-  move_object       $mem_cell -x $offset_x -y [round_up_to_nearest [expr $core_ury-10*$grid_height-$noc_mem_height] $grid_height]
+  move_object       $mem_cell -x $offset_x -y [expr $core_lly+[round_up_to_nearest [expr $core_height-10*$grid_height-$noc_mem_height] $grid_height]]
   set_attribute     $mem_cell orientation $ori
   set_fixed_objects $mem_cell
 
   for {set j 1} {$j < 3} {incr j} {
     set                mem_cell [get_cells "mem_link_[expr $j+$i*4]__link"]
-    move_object       $mem_cell -x $offset_x -y [round_up_to_nearest [expr $ver_gap+(3-$j)*($pod_height+$pod_gap)-0.5*$pod_gap-0.5*$noc_mem_height] $grid_height]
+    move_object       $mem_cell -x $offset_x -y [expr $core_lly+[round_up_to_nearest [expr $ver_gap+(3-$j)*($pod_height+$pod_gap)-0.5*$pod_gap-0.5*$noc_mem_height] $grid_height]]
     set_attribute     $mem_cell orientation $ori
     set_fixed_objects $mem_cell
   }
 
-  set offset_x [expr {$i == 0} ? {[expr 100.5*$grid_width]} : {[expr 667*$grid_width]}]
+  set offset_x [expr {$i == 0} ? {[expr $core_llx+100.5*$grid_width]} : {[expr $core_llx+667*$grid_width]}]
   set ori [expr {$i == 0} ? {"MY"} : {"R0"}]
   set                mem_cell [get_cells "mem_link_[expr 3+$i*4]__link"]
-  move_object       $mem_cell -x $offset_x -y [expr 10*$grid_height]
+  move_object       $mem_cell -x $offset_x -y [expr $core_lly+10*$grid_height]
   set_attribute     $mem_cell orientation $ori
   set_fixed_objects $mem_cell
 }
@@ -97,23 +102,23 @@ for {set i 0} {$i < 2} {incr i} {
 
 # noc_io placement
 set                io_cell [get_cells "io_link"]
-move_object       $io_cell -x [round_up_to_nearest 5000 $grid_width] -y [round_up_to_nearest [expr $core_ury-160] $grid_height]
+move_object       $io_cell -x [expr $core_llx+[round_up_to_nearest 5000 $grid_width]] -y [expr $core_lly+[round_up_to_nearest [expr $core_height-160] $grid_height]]
 set_attribute     $io_cell orientation R0
 set_fixed_objects $io_cell
 
 
 
 # sdr placement
-set noc_io_west_x  [round_down_to_nearest [expr $hor_gap+$bp_width+$noc_mem_width-$grid_width] $grid_width]
-set noc_io_east_x  [round_up_to_nearest [expr $core_urx-$hor_gap-$cgra_width-$noc_mem_width+$grid_width] $grid_width]
+set noc_io_west_x  [expr $core_llx+[round_down_to_nearest [expr $hor_gap+$bp_width+$noc_mem_width-$grid_width] $grid_width]]
+set noc_io_east_x  [expr $core_llx+[round_up_to_nearest [expr $core_width-$hor_gap-$cgra_width-$noc_mem_width+$grid_width] $grid_width]]
 
 set                WEST_cell [get_cells "west_link"]
-move_object       $WEST_cell -x [expr $noc_io_west_x-$noc_mem_width] -y [round_up_to_nearest [expr $core_ury-$ver_gap-$vcache_height-$tile_height+$grid_height] $grid_height]
+move_object       $WEST_cell -x [expr $noc_io_west_x-$noc_mem_width] -y [expr $core_lly+[round_up_to_nearest [expr $core_height-$ver_gap-$vcache_height-$tile_height+$grid_height] $grid_height]]
 set_attribute     $WEST_cell orientation R0
 set_fixed_objects $WEST_cell
 
 set                NORTH_cell [get_cells "north_link"]
-move_object       $NORTH_cell -x [expr 300*$grid_width] -y [round_up_to_nearest [expr $core_ury-$ver_gap+$grid_height] $grid_height]
+move_object       $NORTH_cell -x [expr $core_llx+300*$grid_width] -y [expr $core_lly+[round_up_to_nearest [expr $core_height-$ver_gap+$grid_height] $grid_height]]
 set_attribute     $NORTH_cell orientation R0
 set_fixed_objects $NORTH_cell
 
@@ -124,19 +129,19 @@ for {set i 0} {$i < 2} {incr i} {
   set offset_x [expr {$i == 0} ? {$noc_io_west_x} : {$noc_io_east_x}]
   set ori [expr {$i == 0} ? {"MY"} : {"R0"}]
   set                wh_cell [get_cells "wh_link_[expr 0+$i*5]__link"]
-  move_object       $wh_cell -x $offset_x -y [round_up_to_nearest [expr $core_ury-$ver_gap-$vcache_height+2*$grid_height] $grid_height]
+  move_object       $wh_cell -x $offset_x -y [expr $core_lly+[round_up_to_nearest [expr $core_height-$ver_gap-$vcache_height+2*$grid_height] $grid_height]]
   set_attribute     $wh_cell orientation $ori
   set_fixed_objects $wh_cell
 
   for {set j 1} {$j < 4} {incr j} {
     set                wh_cell [get_cells "wh_link_[expr $j+$i*5]__link"]
-    move_object       $wh_cell -x $offset_x -y [round_up_to_nearest [expr $ver_gap+(4-$j)*($pod_height+$pod_gap)-0.5*$pod_gap-0.5*$noc_mem_height] $grid_height]
+    move_object       $wh_cell -x $offset_x -y [expr $core_lly+[round_up_to_nearest [expr $ver_gap+(4-$j)*($pod_height+$pod_gap)-0.5*$pod_gap-0.5*$noc_mem_height] $grid_height]]
     set_attribute     $wh_cell orientation $ori
     set_fixed_objects $wh_cell
   }
 
   set                wh_cell [get_cells "wh_link_[expr 4+$i*5]__link"]
-  move_object       $wh_cell -x $offset_x -y [round_down_to_nearest [expr $ver_gap+$vcache_height-$noc_mem_height-$grid_height] $grid_height]
+  move_object       $wh_cell -x $offset_x -y [expr $core_lly+[round_down_to_nearest [expr $ver_gap+$vcache_height-$noc_mem_height-$grid_height] $grid_height]]
   set_attribute     $wh_cell orientation $ori
   set_fixed_objects $wh_cell
 }
