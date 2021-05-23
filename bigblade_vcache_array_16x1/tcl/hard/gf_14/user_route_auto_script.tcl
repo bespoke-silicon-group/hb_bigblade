5f23db207e60f77ab6e0b3bf9be8a6423fb99040
Tommy Jung <46542701+tommydcjung@users.noreply.github.com>
2021-05-23 12:56:10 -0700

    DRC pin access routing guide (#76)
    * manycore array pin access routing guide
    
    * vc

--- bigblade_vcache_array_16x1/tcl/hard/gf_14/user_route_auto_script.tcl
+++ bigblade_vcache_array_16x1/tcl/hard/gf_14/user_route_auto_script.tcl
@@ -1,7 +1,13 @@
 
+set vc_cells [get_cells "vc_y_*__vc_x_*__vc"]
+
+# pin access guide
+derive_pin_access_routing_guides -cells $vc_cells -layers {K1 K3} -x_width [expr 0.084*4] -y_width 0.001
+
+
+# route group
 set all_nets [list]
 
-set vc_cells [get_cells "vc_y_*__vc_x_*__vc"]
 append_to_collection all_nets [get_nets -of [get_pins -of $vc_cells -filter "name=~reset*"]]
 append_to_collection all_nets [get_nets -of [get_pins -of $vc_cells -filter "name=~wh_link_sif*"]]
 append_to_collection all_nets [get_nets -of [get_pins -of $vc_cells -filter "name=~ver_link_sif*"]]
