7832573f2d71173b8d4e4b50b962380570376afe
tj <tommy.dc.jung@gmail.com>
2021-05-23 00:23:54 -0700

    manycore array pin access routing guide

--- bigblade_manycore_tile_array_16x2/tcl/hard/gf_14/user_route_auto_script.tcl
+++ bigblade_manycore_tile_array_16x2/tcl/hard/gf_14/user_route_auto_script.tcl
@@ -1,6 +1,17 @@
-set all_nets [list]
 
 set tile_cells [get_cells "y_*__x_*__tile" ]
+
+
+# pin access guide
+derive_pin_access_routing_guides -cells $tile_cells -layers {K1 K3} -x_width [expr 0.084*4] -y_width 0.001
+
+
+
+
+
+#  route group
+set all_nets [list]
+
 append_to_collection all_nets [get_nets -of [get_pins -of $tile_cells -filter "name=~reset*"]]
 append_to_collection all_nets [get_nets -of [get_pins -of $tile_cells -filter "name=~link_*"]]
 append_to_collection all_nets [get_nets -of [get_pins -of $tile_cells -filter "name=~ruche_link*"]]
