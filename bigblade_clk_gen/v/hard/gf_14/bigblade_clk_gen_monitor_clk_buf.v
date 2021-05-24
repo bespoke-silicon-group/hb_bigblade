4e5b3558403d3e53d768a57e6121a2d9f2957731
Scott Davidson <scottdavidson1992@gmail.com>
2021-05-24 14:30:12 -0700

    Adding a hardened x1 buffer before the x4 hardened buffer

--- bigblade_clk_gen/v/hard/gf_14/bigblade_clk_gen_monitor_clk_buf.v
+++ bigblade_clk_gen/v/hard/gf_14/bigblade_clk_gen_monitor_clk_buf.v
@@ -3,6 +3,8 @@ module bigblade_clk_gen_monitor_clk_buf
 , output logic o
 );
 
-  SC7P5T_CKBUFX4_SSC14R buf_BSG_DONT_TOUCH (.CLK(i), .Z(o));
+  wire n0;
+  SC7P5T_CKBUFX1_SSC14R bufx1_BSG_DONT_TOUCH (.CLK(i),  .Z(n0));
+  SC7P5T_CKBUFX4_SSC14R bufx4_BSG_DONT_TOUCH (.CLK(n0), .Z(o));
 
 endmodule // bigblade_clk_gen_monitor_clk_buf
