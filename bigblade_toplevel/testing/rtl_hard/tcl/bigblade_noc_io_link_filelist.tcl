f1e6d743dc203498aa1debe58635d4a053666f1c
Max Ruttenberg <mrutt@cs.washington.edu>
2021-05-25 13:20:21 -0700

    [bigblade_toplevel] moves rtl hard filelists to relevant testing directory

--- bigblade_toplevel/testing/rtl_hard/tcl/bigblade_noc_io_link_filelist.tcl
+++ bigblade_toplevel/testing/rtl_hard/tcl/bigblade_noc_io_link_filelist.tcl
@@ -24,7 +24,6 @@ set BIGBLADE_NOC_IO_LINK_SOURCE_FILES [join "
     $bsg_designs_target_dir/../bigblade_noc_io_link/v/hard/gf_14/bsg_mem/bsg_mem_1r1w_sync.v
     $common_dir/v/hard/gf_14/bsg_link_delay_line.v
     $basejump_stl_dir/hard/gf_14/bsg_misc/bsg_mux.v
-    $basejump_stl_dir/hard/gf_14/bsg_misc/bsg_mux.v
     $basejump_stl_dir/hard/gf_14/bsg_link/bsg_link_osdr_phy.v
     $basejump_stl_dir/hard/gf_14/bsg_link/bsg_link_isdr_phy.v
     $basejump_stl_dir/hard/gf_14/bsg_async/bsg_sync_sync.v
