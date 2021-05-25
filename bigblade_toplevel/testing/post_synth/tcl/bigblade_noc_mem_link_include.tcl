5084a4bae956b498f99b081b2649b2715f51bd61
Max Ruttenberg <mrutt@cs.washington.edu>
2021-05-25 14:31:37 -0700

    [bigblade_toplevel] refactors rtl_hard and post_synth to use libraries for ip blocks

--- bigblade_toplevel/testing/post_synth/tcl/bigblade_noc_mem_link_include.tcl
+++ bigblade_toplevel/testing/post_synth/tcl/bigblade_noc_mem_link_include.tcl
@@ -13,7 +13,8 @@ set bsg_package       $::env(BSG_PACKAGE)
 set bsg_pinout        $::env(BSG_PINOUT)
 set bsg_padmapping    $::env(BSG_PADMAPPING)
 
-set BSG_MANYCORE_TILE_SOURCE_FILES [join "
-  $bsg_manycore_dir/v/bsg_manycore_tile_compute_ruche.v
-  $bsg_manycore_dir/v/bsg_manycore_tile_vcache.v
+set BIGBLADE_NOC_MEM_LINK_INCLUDE_PATHS [ join "
+    $basejump_stl_dir/bsg_misc
+    $basejump_stl_dir/hard/gf_14/bsg_mem
 "]
+
