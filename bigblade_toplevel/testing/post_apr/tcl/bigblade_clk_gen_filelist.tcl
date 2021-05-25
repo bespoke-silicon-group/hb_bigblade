12c2d6ffc2e1b0ec6265302000e5e3572170264b
Max Ruttenberg <mrutt@cs.washington.edu>
2021-05-25 16:30:18 -0700

    [bigblade_toplevel] post_apr testbench

---
+++ bigblade_toplevel/testing/post_apr/tcl/bigblade_clk_gen_filelist.tcl
@@ -0,0 +1,24 @@
+#------------------------------------------------------------
+# Do NOT arbitrarily change the order of files. Some module
+# and macro definitions may be needed by the subsequent files
+#------------------------------------------------------------
+
+set basejump_stl_dir       $::env(BASEJUMP_STL_DIR)
+set bsg_manycore_dir       $::env(BSG_MANYCORE_DIR)
+set bsg_designs_dir        $::env(BSG_DESIGNS_DIR)
+set bsg_designs_target_dir $::env(BSG_DESIGNS_TARGET_DIR)
+set bsg_packaging_dir      $::env(BSG_PACKAGING_DIR)
+
+set bsg_package       $::env(BSG_PACKAGE)
+set bsg_pinout        $::env(BSG_PINOUT)
+set bsg_padmapping    $::env(BSG_PADMAPPING)
+
+# redirect to point to symlinks
+set bsg_designs_target $::env(BSG_DESIGNS_TARGET)
+set bsg_out_dir $::env(TESTING_BSG_OUT_DIR)
+
+set bsg_designs_target_dir "$bsg_out_dir/bigblade_clk_gen/$bsg_designs_target"
+set basejump_stl_dir       "$bsg_designs_target_dir/basejump_stl"
+
+source $::env(HB_BIGBLADE_NETLISTS_DIR)/bigblade_clk_gen-post-synth_filelist.tcl
+
