ddcc4748bc7771c27397f68476d9df52289ed623
Max Ruttenberg <mrutt@cs.washington.edu>
2021-05-25 11:08:20 -0700

    [bigblade_toplevel] rtl hard for rc alpha 3

---
+++ bigblade_toplevel/testing/tcl/bigblade_clk_gen_filelist.tcl
@@ -0,0 +1,32 @@
+#------------------------------------------------------------
+# Do NOT arbitrarily change the order of files. Some module
+# and macro definitions may be needed by the subsequent files
+#------------------------------------------------------------
+
+set basejump_stl_dir       $::env(BASEJUMP_STL_DIR)
+set bsg_manycore_dir       $::env(BSG_MANYCORE_DIR)
+set bsg_designs_dir        $::env(BSG_DESIGNS_DIR)
+set bsg_designs_target_dir $::env(BSG_DESIGNS_TARGET_DIR)
+set bsg_packaging_dir      $::env(BSG_PACKAGING_DIR)
+
+set bsg_package       $::env(BSG_PACKAGE)
+set bsg_pinout        $::env(BSG_PINOUT)
+set bsg_padmapping    $::env(BSG_PADMAPPING)
+
+# redirect to point to symlinks
+set bsg_designs_target $::env(BSG_DESIGNS_TARGET)
+set bsg_out_dir $::env(TESTING_BSG_OUT_DIR)
+
+set bsg_designs_target_dir "$bsg_out_dir/bigblade_clk_gen/$bsg_designs_target"
+set basejump_stl_dir       "$bsg_designs_target_dir/basejump_stl"
+
+set BIGBLADE_CLK_GEN_SOURCE_FILES [join "   
+  $basejump_stl_dir/hard/gf_14/bsg_async/bsg_launch_sync_sync.v
+  $basejump_stl_dir/hard/gf_14/bsg_async/bsg_sync_sync.v
+  $basejump_stl_dir/hard/gf_14/bsg_clk_gen/bsg_clk_gen_osc.v
+  $basejump_stl_dir/hard/gf_14/bsg_misc/bsg_mux.v
+
+  $basejump_stl_dir/hard/gf_14/bsg_clk_gen/bsg_rp_clk_gen_atomic_delay_tuner.v
+  $basejump_stl_dir/hard/gf_14/bsg_clk_gen/bsg_rp_clk_gen_coarse_delay_tuner.v
+  $basejump_stl_dir/hard/gf_14/bsg_clk_gen/bsg_rp_clk_gen_fine_delay_tuner.v
+"]
