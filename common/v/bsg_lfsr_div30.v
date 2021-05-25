7976fe0c5f6ca093cb2320b557117206ae167a31
Paul Gao <gaozihou@uw.edu>
2021-05-25 11:06:08 -0700

    lfsr div30 explicit reset (#83)

--- common/v/bsg_lfsr_div30.v
+++ common/v/bsg_lfsr_div30.v
@@ -10,13 +10,22 @@ module bsg_lfsr_div30
   logic out_r;
   assign clk_div_o = out_r;
 
+  // synopsys sync_set_reset "reset_i"
   always_ff @(posedge clk_i)
   begin
+    if (reset_i)
+      begin
+        data_r[1] <= 1'b1;
+        out_r     <= 1'b0;
+      end
+    else
+      begin
+        data_r[1] <= data_r[2];
+        out_r     <= (~(| data_r[2:0]) ^ out_r);
+      end
     data_r[0] <= data_r[1];
-    data_r[1] <= reset_i | data_r[2];
     data_r[2] <= data_r[0] ^ data_r[3];
     data_r[3] <= data_r[0];
-    out_r     <= ~reset_i & (~(| data_r[2:0]) ^ out_r);
   end
 
 endmodule
