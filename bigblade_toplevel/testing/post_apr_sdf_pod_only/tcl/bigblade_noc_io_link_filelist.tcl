ddca7456c4a78534e3c132f2e583b7526c073e1d
tj <tommy.dc.jung@gmail.com>
2021-05-27 23:36:03 -0700

    post_apt_sdf_pod_only

---
+++ bigblade_toplevel/testing/post_apr_sdf_pod_only/tcl/bigblade_noc_io_link_filelist.tcl
@@ -0,0 +1,23 @@
+#------------------------------------------------------------
+# Do NOT arbitrarily change the order of files. Some module
+# and macro definitions may be needed by the subsequent files
+#------------------------------------------------------------
+
+set basejump_stl_dir       $::env(BASEJUMP_STL_DIR)
+set bsg_manycore_dir       $::env(BSG_MANYCORE_DIR)
+set bsg_designs_dir        $::env(BSG_DESIGNS_DIR)
+set bsg_designs_target_dir $::env(BSG_DESIGNS_TARGET_DIR)
+set bsg_packaging_dir      $::env(BSG_PACKAGING_DIR)
+
+set bsg_package       $::env(BSG_PACKAGE)
+set bsg_pinout        $::env(BSG_PINOUT)
+set bsg_padmapping    $::env(BSG_PADMAPPING)
+
+# redirect to point to symlinks
+set bsg_designs_target $::env(BSG_DESIGNS_TARGET)
+set bsg_out_dir $::env(TESTING_BSG_OUT_DIR)
+
+set basejump_stl_dir "$bsg_out_dir/bigblade_noc_io_link/$bsg_designs_target/basejump_stl"
+set common_dir       "$bsg_out_dir/bigblade_noc_io_link/$bsg_designs_target/../common"
+
+source $::env(HB_BIGBLADE_NETLISTS_DIR)/bigblade_noc_io_link-post-synth_filelist.tcl
