8826ccf7fdbb7ecf91b4f993e77a4134efd5fae9
tj <tommy.dc.jung@gmail.com>
2021-05-30 15:56:37 -0700

    turn on spmd loder verbose

--- bigblade_toplevel/testing/v/bsg_gateway_chip_core_complex.v
+++ bigblade_toplevel/testing/v/bsg_gateway_chip_core_complex.v
@@ -1,3 +1,7 @@
+`ifndef SPMD_VERBOSE
+  `define SPMD_VERBOSE 0
+`endif
+
 
 module bsg_gateway_chip_core_complex
 
@@ -70,7 +74,7 @@ module bsg_gateway_chip_core_complex
     ,.y_cord_width_p(hb_y_cord_width_gp)
     ,.io_x_cord_p(7'b0001111)
     ,.io_y_cord_p(7'b0001000)
-    ,.verbose_p(0)
+    ,.verbose_p(`SPMD_VERBOSE)
   ) host (
     .clk_i(mc_clk_i)
     ,.reset_i(~tag_trace_done_i)
