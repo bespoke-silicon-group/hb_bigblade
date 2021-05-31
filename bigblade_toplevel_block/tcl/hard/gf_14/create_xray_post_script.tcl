bc0311b448dd7cd55facfdcf7c5aec6412bfc5c6
Paul Gao <gaozihou@uw.edu>
2021-05-30 20:14:53 -0700

    fix typo. create internally generated clocks after create xray

--- bigblade_toplevel_block/tcl/hard/gf_14/create_xray_post_script.tcl
+++ bigblade_toplevel_block/tcl/hard/gf_14/create_xray_post_script.tcl
@@ -3,6 +3,37 @@ source -echo -verbose $::env(BSG_DESIGNS_TARGET_DIR)/../common/bsg_chip_misc.tcl
 
 remove_clocks [get_clocks {ext_io_clk ext_noc_clk ext_mc_clk*}]
 
+
+
+set io_clk_period_ps          800.0 ;# 1.25 GHz
+set io_clk_uncertainty_ps     20
+
+for {set i 0} {$i < 8} {incr i} {
+  for {set j 0} {$j < 2} {incr j} {
+    set io_clk_name   "mem_link_${i}_${j}_io_clk"
+    create_clock -period $io_clk_period_ps -name $io_clk_name [get_pins "mem_link*${i}*link/ddr_link*${j}*link/clk_gen_io/clk_gen_inst/mux_inst/*/Z"]
+    set_clock_uncertainty $io_clk_uncertainty_ps  [get_clocks $io_clk_name]
+  }
+}
+
+for {set j 0} {$j < 2} {incr j} {
+  set io_clk_name   "io_link_${j}_io_clk"
+  create_clock -period $io_clk_period_ps -name $io_clk_name [get_pins "io_link/ddr_link*${j}*link/clk_gen_io/clk_gen_inst/mux_inst/*/Z"]
+  set_clock_uncertainty $io_clk_uncertainty_ps  [get_clocks $io_clk_name]
+}
+
+for {set i 0} {$i < 8} {incr i} {
+  set wh_master_clk_name   "mem_link_${i}_master_clk"
+  create_clock -period 1000 -name $wh_master_clk_name [get_pins "mem_link*${i}*link/ddr_link*0*link/clk_gen_noc/clk_gen_inst/mux_inst/*/Z"]
+  set_clock_uncertainty 20 [get_clocks $wh_master_clk_name]
+}
+
+set wh_master_clk_name   "io_link_master_clk"
+create_clock -period 1000 -name $wh_master_clk_name [get_pins "io_link/ddr_link*0*link/clk_gen_noc/clk_gen_inst/mux_inst/*/Z"]
+set_clock_uncertainty 20 [get_clocks $wh_master_clk_name]
+
+
+
 bsg_chip_derate_cells
 
 foreach_in_collection cell [get_cells -quiet -hier -filter "(ref_name=~gf14_*)&&(full_name=~*downstream*harden*fifo*)"] {
