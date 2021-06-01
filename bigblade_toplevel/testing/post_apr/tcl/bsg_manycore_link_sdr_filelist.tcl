357e6e620b86af1093ddef58ed1360425bc61d70
Dustin Richmond <dustinar@uw.edu>
2021-06-01 16:18:49 -0700

    Shoe-horned (?) new per-module approach into old approach

--- bigblade_toplevel/testing/post_apr/tcl/bsg_manycore_link_sdr_filelist.tcl
+++ bigblade_toplevel/testing/post_apr/tcl/bsg_manycore_link_sdr_filelist.tcl
@@ -20,4 +20,46 @@ set bsg_out_dir $::env(TESTING_BSG_OUT_DIR)
 set basejump_stl_dir "$bsg_out_dir/bsg_manycore_link_sdr/$bsg_designs_target/basejump_stl"
 set common_dir       "$bsg_out_dir/bsg_manycore_link_sdr/$bsg_designs_target/../common"
 
-source $::env(HB_BIGBLADE_NETLISTS_DIR)/bsg_manycore_link_sdr-post-apr_filelist.tcl
+# Historically BSG_MANYCORE_LINK_SDR_SOURCE_FILES were all specified in
+# one file. This has changed, and they are now per-module, as
+# below. However, for backward compatibility we've reused the
+# variable.
+
+# Old Way:
+# source $::env(HB_BIGBLADE_NETLISTS_DIR)/bsg_manycore_link_sdr-post-apr_filelist.tcl
+
+# New Way: For now, include each of the sub-modules.
+set BSG_MANYCORE_LINK_SDR_SOURCE_FILES []
+source $::env(HB_BIGBLADE_NETLISTS_DIR)/bsg_manycore_link_ruche_to_sdr_east-post-apr_filelist.tcl
+set BSG_MANYCORE_LINK_SDR_SOURCE_FILES [concat $BSG_MANYCORE_LINK_SDR_SOURCE_FILES $BSG_MANYCORE_LINK_RUCHE_TO_SDR_EAST_SOURCE_FILES]
+
+source $::env(HB_BIGBLADE_NETLISTS_DIR)/bsg_manycore_link_ruche_to_sdr_west-post-apr_filelist.tcl
+set BSG_MANYCORE_LINK_SDR_SOURCE_FILES [concat $BSG_MANYCORE_LINK_SDR_SOURCE_FILES $BSG_MANYCORE_LINK_RUCHE_TO_SDR_WEST_SOURCE_FILES]
+
+source $::env(HB_BIGBLADE_NETLISTS_DIR)/bsg_manycore_link_to_sdr_north-post-apr_filelist.tcl
+set BSG_MANYCORE_LINK_SDR_SOURCE_FILES [concat $BSG_MANYCORE_LINK_SDR_SOURCE_FILES $BSG_MANYCORE_LINK_TO_SDR_NORTH_SOURCE_FILES]
+
+source $::env(HB_BIGBLADE_NETLISTS_DIR)/bsg_manycore_link_to_sdr_north_row-post-apr_filelist.tcl
+set BSG_MANYCORE_LINK_SDR_SOURCE_FILES [concat $BSG_MANYCORE_LINK_SDR_SOURCE_FILES $BSG_MANYCORE_LINK_TO_SDR_NORTH_ROW_SOURCE_FILES]
+
+source $::env(HB_BIGBLADE_NETLISTS_DIR)/bsg_manycore_link_to_sdr_south-post-apr_filelist.tcl
+set BSG_MANYCORE_LINK_SDR_SOURCE_FILES [concat $BSG_MANYCORE_LINK_SDR_SOURCE_FILES $BSG_MANYCORE_LINK_TO_SDR_SOUTH_SOURCE_FILES]
+
+source $::env(HB_BIGBLADE_NETLISTS_DIR)/bsg_manycore_link_to_sdr_south_row-post-apr_filelist.tcl
+set BSG_MANYCORE_LINK_SDR_SOURCE_FILES [concat $BSG_MANYCORE_LINK_SDR_SOURCE_FILES $BSG_MANYCORE_LINK_TO_SDR_SOUTH_ROW_SOURCE_FILES]
+
+source $::env(HB_BIGBLADE_NETLISTS_DIR)/bsg_manycore_link_wh_to_sdr_ne-post-apr_filelist.tcl
+set BSG_MANYCORE_LINK_SDR_SOURCE_FILES [concat $BSG_MANYCORE_LINK_SDR_SOURCE_FILES $BSG_MANYCORE_LINK_WH_TO_SDR_NE_SOURCE_FILES]
+
+source $::env(HB_BIGBLADE_NETLISTS_DIR)/bsg_manycore_link_wh_to_sdr_nw-post-apr_filelist.tcl
+set BSG_MANYCORE_LINK_SDR_SOURCE_FILES [concat $BSG_MANYCORE_LINK_SDR_SOURCE_FILES $BSG_MANYCORE_LINK_WH_TO_SDR_NW_SOURCE_FILES]
+
+source $::env(HB_BIGBLADE_NETLISTS_DIR)/bsg_manycore_link_wh_to_sdr_se-post-apr_filelist.tcl
+set BSG_MANYCORE_LINK_SDR_SOURCE_FILES [concat $BSG_MANYCORE_LINK_SDR_SOURCE_FILES $BSG_MANYCORE_LINK_WH_TO_SDR_SE_SOURCE_FILES]
+
+source $::env(HB_BIGBLADE_NETLISTS_DIR)/bsg_manycore_link_wh_to_sdr_sw-post-apr_filelist.tcl
+set BSG_MANYCORE_LINK_SDR_SOURCE_FILES [concat $BSG_MANYCORE_LINK_SDR_SOURCE_FILES $BSG_MANYCORE_LINK_WH_TO_SDR_SW_SOURCE_FILES]
+
+source $::env(HB_BIGBLADE_NETLISTS_DIR)/bsg_manycore_pod_row_sdr-post-apr_filelist.tcl
+set BSG_MANYCORE_LINK_SDR_SOURCE_FILES [concat $BSG_MANYCORE_LINK_SDR_SOURCE_FILES $BIGBLADE_POD_ROW_SOURCE_FILES]
+
--- bigblade_toplevel/testing/post_synth/tcl/bsg_manycore_link_sdr_filelist.tcl
+++ bigblade_toplevel/testing/post_synth/tcl/bsg_manycore_link_sdr_filelist.tcl
@@ -20,4 +20,45 @@ set bsg_out_dir $::env(TESTING_BSG_OUT_DIR)
 set basejump_stl_dir "$bsg_out_dir/bsg_manycore_link_sdr/$bsg_designs_target/basejump_stl"
 set common_dir       "$bsg_out_dir/bsg_manycore_link_sdr/$bsg_designs_target/../common"
 
-source $::env(HB_BIGBLADE_NETLISTS_DIR)/bsg_manycore_link_sdr-post-synth_filelist.tcl
+# Historically BSG_MANYCORE_LINK_SDR_SOURCE_FILES were all specified in
+# one file. This has changed, and they are now per-module, as
+# below. However, for backward compatibility we've reused the
+# variable.
+
+# Old Way:
+# source $::env(HB_BIGBLADE_NETLISTS_DIR)/bsg_manycore_link_sdr-post-synth_filelist.tcl
+
+# New Way: For now, include each of the sub-modules.
+set BSG_MANYCORE_LINK_SDR_SOURCE_FILES []
+source $::env(HB_BIGBLADE_NETLISTS_DIR)/bsg_manycore_link_ruche_to_sdr_east-post-synth_filelist.tcl
+set BSG_MANYCORE_LINK_SDR_SOURCE_FILES [concat $BSG_MANYCORE_LINK_SDR_SOURCE_FILES $BSG_MANYCORE_LINK_RUCHE_TO_SDR_EAST_SOURCE_FILES]
+
+source $::env(HB_BIGBLADE_NETLISTS_DIR)/bsg_manycore_link_ruche_to_sdr_west-post-synth_filelist.tcl
+set BSG_MANYCORE_LINK_SDR_SOURCE_FILES [concat $BSG_MANYCORE_LINK_SDR_SOURCE_FILES $BSG_MANYCORE_LINK_RUCHE_TO_SDR_WEST_SOURCE_FILES]
+
+source $::env(HB_BIGBLADE_NETLISTS_DIR)/bsg_manycore_link_to_sdr_north-post-synth_filelist.tcl
+set BSG_MANYCORE_LINK_SDR_SOURCE_FILES [concat $BSG_MANYCORE_LINK_SDR_SOURCE_FILES $BSG_MANYCORE_LINK_TO_SDR_NORTH_SOURCE_FILES]
+
+source $::env(HB_BIGBLADE_NETLISTS_DIR)/bsg_manycore_link_to_sdr_north_row-post-synth_filelist.tcl
+set BSG_MANYCORE_LINK_SDR_SOURCE_FILES [concat $BSG_MANYCORE_LINK_SDR_SOURCE_FILES $BSG_MANYCORE_LINK_TO_SDR_NORTH_ROW_SOURCE_FILES]
+
+source $::env(HB_BIGBLADE_NETLISTS_DIR)/bsg_manycore_link_to_sdr_south-post-synth_filelist.tcl
+set BSG_MANYCORE_LINK_SDR_SOURCE_FILES [concat $BSG_MANYCORE_LINK_SDR_SOURCE_FILES $BSG_MANYCORE_LINK_TO_SDR_SOUTH_SOURCE_FILES]
+
+source $::env(HB_BIGBLADE_NETLISTS_DIR)/bsg_manycore_link_to_sdr_south_row-post-synth_filelist.tcl
+set BSG_MANYCORE_LINK_SDR_SOURCE_FILES [concat $BSG_MANYCORE_LINK_SDR_SOURCE_FILES $BSG_MANYCORE_LINK_TO_SDR_SOUTH_ROW_SOURCE_FILES]
+
+source $::env(HB_BIGBLADE_NETLISTS_DIR)/bsg_manycore_link_wh_to_sdr_ne-post-synth_filelist.tcl
+set BSG_MANYCORE_LINK_SDR_SOURCE_FILES [concat $BSG_MANYCORE_LINK_SDR_SOURCE_FILES $BSG_MANYCORE_LINK_WH_TO_SDR_NE_SOURCE_FILES]
+
+source $::env(HB_BIGBLADE_NETLISTS_DIR)/bsg_manycore_link_wh_to_sdr_nw-post-synth_filelist.tcl
+set BSG_MANYCORE_LINK_SDR_SOURCE_FILES [concat $BSG_MANYCORE_LINK_SDR_SOURCE_FILES $BSG_MANYCORE_LINK_WH_TO_SDR_NW_SOURCE_FILES]
+
+source $::env(HB_BIGBLADE_NETLISTS_DIR)/bsg_manycore_link_wh_to_sdr_se-post-synth_filelist.tcl
+set BSG_MANYCORE_LINK_SDR_SOURCE_FILES [concat $BSG_MANYCORE_LINK_SDR_SOURCE_FILES $BSG_MANYCORE_LINK_WH_TO_SDR_SE_SOURCE_FILES]
+
+source $::env(HB_BIGBLADE_NETLISTS_DIR)/bsg_manycore_link_wh_to_sdr_sw-post-synth_filelist.tcl
+set BSG_MANYCORE_LINK_SDR_SOURCE_FILES [concat $BSG_MANYCORE_LINK_SDR_SOURCE_FILES $BSG_MANYCORE_LINK_WH_TO_SDR_SW_SOURCE_FILES]
+
+source $::env(HB_BIGBLADE_NETLISTS_DIR)/bsg_manycore_pod_row_sdr-post-synth_filelist.tcl
+set BSG_MANYCORE_LINK_SDR_SOURCE_FILES [concat $BSG_MANYCORE_LINK_SDR_SOURCE_FILES $BIGBLADE_POD_ROW_SOURCE_FILES]
