068f1318e5d9ade26f770f97e0bad9bd272b2722
Dustin Richmond <dustinar@uw.edu>
2021-06-01 17:58:52 -0700

    Update filelists for SDR/Tile filelists (#102)
    * Shoe-horned (?) new per-module approach into old approach
    
    * Fix bsg_tiehilo library naming
    
    * Revert synth change (accidental)

--- bigblade_toplevel/testing/post_apr/tcl/bsg_manycore_tile_filelist.tcl
+++ bigblade_toplevel/testing/post_apr/tcl/bsg_manycore_tile_filelist.tcl
@@ -15,4 +15,25 @@ set bsg_padmapping    $::env(BSG_PADMAPPING)
 
 set bsg_out_dir       $::env(TESTING_BSG_OUT_DIR)
 
-source $::env(HB_BIGBLADE_NETLISTS_DIR)/bsg_manycore_tile-post-apr_filelist.tcl
+# Historically BSG_MANYCORE_TILE_SOURCE_FILES were all specified in
+# one file. This has changed, and they are now per-module, as
+# below. However, for backward compatibility we've reused the
+# variable.
+
+# Old Way:
+# source $::env(HB_BIGBLADE_NETLISTS_DIR)/bsg_manycore_tile-post-apr_filelist.tcl
+
+# New Way: For now, include each of the sub-modules.
+set BSG_MANYCORE_TILE_SOURCE_FILES []
+source $::env(HB_BIGBLADE_NETLISTS_DIR)/bsg_manycore_tile_compute_array_ruche-post-apr_filelist.tcl
+set BSG_MANYCORE_TILE_SOURCE_FILES [concat $BSG_MANYCORE_TILE_SOURCE_FILES $BIGBLADE_MANYCORE_TILE_ARRAY_16X2_SOURCE_FILES]
+
+source $::env(HB_BIGBLADE_NETLISTS_DIR)/bsg_manycore_tile_compute_ruche-post-apr_filelist.tcl
+set BSG_MANYCORE_TILE_SOURCE_FILES [concat $BSG_MANYCORE_TILE_SOURCE_FILES $BIGBLADE_MANYCORE_TILE_SOURCE_FILES]
+
+source $::env(HB_BIGBLADE_NETLISTS_DIR)/bsg_manycore_tile_vcache_array-post-apr_filelist.tcl
+set BSG_MANYCORE_TILE_SOURCE_FILES [concat $BSG_MANYCORE_TILE_SOURCE_FILES $BIGBLADE_VCACHE_ARRAY_16X1_SOURCE_FILES]
+
+source $::env(HB_BIGBLADE_NETLISTS_DIR)/bsg_manycore_tile_vcache-post-apr_filelist.tcl
+set BSG_MANYCORE_TILE_SOURCE_FILES [concat $BSG_MANYCORE_TILE_SOURCE_FILES $BIGBLADE_VCACHE_SOURCE_FILES]
+
