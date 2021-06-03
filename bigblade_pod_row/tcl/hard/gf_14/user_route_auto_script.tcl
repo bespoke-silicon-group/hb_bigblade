6603866c6997cd3df3ade1f1283619865bccb289
tj <tommy.dc.jung@gmail.com>
2021-06-03 12:06:38 -0700

    route async disable nets

--- bigblade_pod_row/tcl/hard/gf_14/user_route_auto_script.tcl
+++ bigblade_pod_row/tcl/hard/gf_14/user_route_auto_script.tcl
@@ -21,12 +21,14 @@ append_to_collection all_nets [get_nets -of [get_pins -of $sdr_north_cells -filt
 append_to_collection all_nets [get_nets -of [get_pins -of $sdr_north_cells -filter "name=~io*"]]
 append_to_collection all_nets [get_nets -of [get_pins -of $sdr_north_cells -filter "name=~tag_data_i"]]
 append_to_collection all_nets [get_nets -of [get_pins -of $sdr_north_cells -filter "name=~node_id_offset_i*"]]
+append_to_collection all_nets [get_nets -of [get_pins -of $sdr_north_cells -filter "name=~async*disable*"]]
 
 # SDR south
 set sdr_south_cells [get_cells "sdr_s_x_*__sdr_s"]
 append_to_collection all_nets [get_nets -of [get_pins -of $sdr_south_cells -filter "name=~core_link_sif*"]]
 append_to_collection all_nets [get_nets -of [get_pins -of $sdr_south_cells -filter "name=~async_*reset*"]]
 append_to_collection all_nets [get_nets -of [get_pins -of $sdr_south_cells -filter "name=~io*"]]
+append_to_collection all_nets [get_nets -of [get_pins -of $sdr_south_cells -filter "name=~async*disable*"]]
 
 # SDR horizontal
 set sdr_horiz_cells [get_cells "sdr_*_y_*__sdr_*"]
@@ -36,6 +38,7 @@ append_to_collection all_nets [get_nets -of [get_pins -of $sdr_horiz_cells -filt
 append_to_collection all_nets [get_nets -of [get_pins -of $sdr_horiz_cells -filter "name=~core_reset*"]]
 append_to_collection all_nets [get_nets -of [get_pins -of $sdr_horiz_cells -filter "name=~async*reset*"]]
 append_to_collection all_nets [get_nets -of [get_pins -of $sdr_horiz_cells -filter "name=~io*"]]
+append_to_collection all_nets [get_nets -of [get_pins -of $sdr_horiz_cells -filter "name=~async*disable*"]]
 
 # SDR corner
 set sdr_corner_cells [list]
@@ -51,6 +54,7 @@ append_to_collection all_nets [get_nets -of [get_pins -of $sdr_corner_cells -fil
 append_to_collection all_nets [get_nets -of [get_pins -of $sdr_corner_cells -filter "name=~io_*"]]
 append_to_collection all_nets [get_nets -of [get_pins -of $sdr_corner_cells -filter "name=~tag_data_i"]]
 append_to_collection all_nets [get_nets -of [get_pins -of $sdr_corner_cells -filter "name=~tag_node_id_offset_i*"]]
+append_to_collection all_nets [get_nets -of [get_pins -of $sdr_corner_cells -filter "name=~async*disable*"]]
 
 # route everything
 route_group -nets $all_nets
