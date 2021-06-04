5b67498cc5f6ca1d24883004d16a9cc3df03913d
Dan Petrisko <petrisko@cs.washington.edu>
2021-06-04 04:00:03 -0700

    Updating host coordinate

--- bigblade_bp_unicore/v/bp_cce_to_mc_bridge.sv
+++ bigblade_bp_unicore/v/bp_cce_to_mc_bridge.sv
@@ -390,10 +390,10 @@ module bp_cce_to_mc_bridge
         end
       else
         begin
-          // Hardcoded host at right above this link
+          // Hardcoded host at top right of bigblade
           mmio_out_packet_li.addr   = io_cmd_eva_li.addr;
-          mmio_out_packet_li.y_cord = my_y_i - 1'b1;
-          mmio_out_packet_li.x_cord = my_x_i;
+          mmio_out_packet_li.y_cord = '0;
+          mmio_out_packet_li.x_cord = 31;
         end
 
       case (io_cmd_li.header.msg_type)
