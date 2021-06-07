61dfc3046670162f8fa7eecf52c2529af3991525
Scott Davidson <scottdavidson1992@gmail.com>
2021-06-07 03:36:41 -0700

    Updates to blackbox view to allow ring pin connections

--- bigblade_toplevel/mk/custom_steps.mk
+++ bigblade_toplevel/mk/custom_steps.mk
@@ -7,7 +7,12 @@ generate_black_box:
 	cd $(BSG_DESIGNS_TARGET_DIR)/../bigblade_toplevel_block/bsg_14/current_build/pnr/bsg_chip_block && \
 		$(ICC2_SHELL) -f $(BSG_DESIGNS_TARGET_DIR)/tcl/hard/gf_14/black_box/generate_lef_from_init_design.tcl
 	mkdir -p $(BUILD_DIR)/black_box/$(DESIGN_NAME)
-	mv /tmp/temp_$(DESIGN_NAME).lef $(BUILD_DIR)/black_box/$(DESIGN_NAME)/$(DESIGN_NAME).lef
+	mv /tmp/temp_$(DESIGN_NAME).lef $(BUILD_DIR)/black_box/$(DESIGN_NAME)/$(DESIGN_NAME).lef.orig
+	cp $(BUILD_DIR)/black_box/$(DESIGN_NAME)/$(DESIGN_NAME).lef.orig $(BUILD_DIR)/black_box/$(DESIGN_NAME)/$(DESIGN_NAME).lef
+	sed -i -e '/OBS/,/END/{/LAYER G1/,/LAYER/{/RECT/d}}' $(BUILD_DIR)/black_box/$(DESIGN_NAME)/$(DESIGN_NAME).lef
+	sed -i -e '/OBS/,/END/{/LAYER G2/,/LAYER/{/RECT/d}}' $(BUILD_DIR)/black_box/$(DESIGN_NAME)/$(DESIGN_NAME).lef
+	sed -i -e '/OBS/,/END/{/LAYER G1/d}' $(BUILD_DIR)/black_box/$(DESIGN_NAME)/$(DESIGN_NAME).lef
+	sed -i -e '/OBS/,/END/{/LAYER G2/d}' $(BUILD_DIR)/black_box/$(DESIGN_NAME)/$(DESIGN_NAME).lef
 	cd $(BUILD_DIR)/black_box/$(DESIGN_NAME) && \
 		$(ICC2_LM_SHELL) -f $(BSG_DESIGNS_TARGET_DIR)/tcl/hard/gf_14/black_box/lef2ndm.tcl
 	touch $(BUILD_VPATH)/$@
