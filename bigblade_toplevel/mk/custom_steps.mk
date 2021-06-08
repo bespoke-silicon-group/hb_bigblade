2ac14dd818eb2a29b346333b498c48f1c1bd4189
Scott Davidson <scottdavidson1992@gmail.com>
2021-06-08 14:46:12 -0700

    Adding sed to rip out shapes in black-box lef that we don't need to reduce size (#128)
    * Adding sed to rip out shapes in black-box lef that we don't need to reduce size
    
    * Change order to speedup sed

--- bigblade_toplevel/mk/custom_steps.mk
+++ bigblade_toplevel/mk/custom_steps.mk
@@ -9,10 +9,38 @@ generate_black_box:
 	mkdir -p $(BUILD_DIR)/black_box/$(DESIGN_NAME)
 	mv /tmp/temp_$(DESIGN_NAME).lef $(BUILD_DIR)/black_box/$(DESIGN_NAME)/$(DESIGN_NAME).lef.orig
 	cp $(BUILD_DIR)/black_box/$(DESIGN_NAME)/$(DESIGN_NAME).lef.orig $(BUILD_DIR)/black_box/$(DESIGN_NAME)/$(DESIGN_NAME).lef
+	sed -i -e '/OBS/,/END/{/LAYER T1/,/LAYER/{/RECT/d}}' $(BUILD_DIR)/black_box/$(DESIGN_NAME)/$(DESIGN_NAME).lef
 	sed -i -e '/OBS/,/END/{/LAYER G1/,/LAYER/{/RECT/d}}' $(BUILD_DIR)/black_box/$(DESIGN_NAME)/$(DESIGN_NAME).lef
 	sed -i -e '/OBS/,/END/{/LAYER G2/,/LAYER/{/RECT/d}}' $(BUILD_DIR)/black_box/$(DESIGN_NAME)/$(DESIGN_NAME).lef
+	sed -i -e '/OBS/,/END/{/LAYER C4/,/LAYER/{/RECT/d}}' $(BUILD_DIR)/black_box/$(DESIGN_NAME)/$(DESIGN_NAME).lef
+	sed -i -e '/OBS/,/END/{/LAYER C5/,/LAYER/{/RECT/d}}' $(BUILD_DIR)/black_box/$(DESIGN_NAME)/$(DESIGN_NAME).lef
+	sed -i -e '/OBS/,/END/{/LAYER H1/,/LAYER/{/RECT/d}}' $(BUILD_DIR)/black_box/$(DESIGN_NAME)/$(DESIGN_NAME).lef
+	sed -i -e '/OBS/,/END/{/LAYER H2/,/LAYER/{/RECT/d}}' $(BUILD_DIR)/black_box/$(DESIGN_NAME)/$(DESIGN_NAME).lef
+	sed -i -e '/OBS/,/END/{/LAYER J3/,/LAYER/{/RECT/d}}' $(BUILD_DIR)/black_box/$(DESIGN_NAME)/$(DESIGN_NAME).lef
+	sed -i -e '/OBS/,/END/{/LAYER A4/,/LAYER/{/RECT/d}}' $(BUILD_DIR)/black_box/$(DESIGN_NAME)/$(DESIGN_NAME).lef
+	sed -i -e '/OBS/,/END/{/LAYER CK/,/LAYER/{/RECT/d}}' $(BUILD_DIR)/black_box/$(DESIGN_NAME)/$(DESIGN_NAME).lef
+	sed -i -e '/OBS/,/END/{/LAYER U1/,/LAYER/{/RECT/d}}' $(BUILD_DIR)/black_box/$(DESIGN_NAME)/$(DESIGN_NAME).lef
+	sed -i -e '/OBS/,/END/{/LAYER U2/,/LAYER/{/RECT/d}}' $(BUILD_DIR)/black_box/$(DESIGN_NAME)/$(DESIGN_NAME).lef
+	sed -i -e '/OBS/,/END/{/LAYER U3/,/LAYER/{/RECT/d}}' $(BUILD_DIR)/black_box/$(DESIGN_NAME)/$(DESIGN_NAME).lef
+	sed -i -e '/OBS/,/END/{/LAYER KH/,/LAYER/{/RECT/d}}' $(BUILD_DIR)/black_box/$(DESIGN_NAME)/$(DESIGN_NAME).lef
+	sed -i -e '/OBS/,/END/{/LAYER N1/,/LAYER/{/RECT/d}}' $(BUILD_DIR)/black_box/$(DESIGN_NAME)/$(DESIGN_NAME).lef
+	sed -i -e '/OBS/,/END/{/LAYER HG/,/LAYER/{/RECT/d}}' $(BUILD_DIR)/black_box/$(DESIGN_NAME)/$(DESIGN_NAME).lef
+	sed -i -e '/OBS/,/END/{/LAYER C4/d}' $(BUILD_DIR)/black_box/$(DESIGN_NAME)/$(DESIGN_NAME).lef
+	sed -i -e '/OBS/,/END/{/LAYER C5/d}' $(BUILD_DIR)/black_box/$(DESIGN_NAME)/$(DESIGN_NAME).lef
+	sed -i -e '/OBS/,/END/{/LAYER H1/d}' $(BUILD_DIR)/black_box/$(DESIGN_NAME)/$(DESIGN_NAME).lef
+	sed -i -e '/OBS/,/END/{/LAYER H2/d}' $(BUILD_DIR)/black_box/$(DESIGN_NAME)/$(DESIGN_NAME).lef
 	sed -i -e '/OBS/,/END/{/LAYER G1/d}' $(BUILD_DIR)/black_box/$(DESIGN_NAME)/$(DESIGN_NAME).lef
 	sed -i -e '/OBS/,/END/{/LAYER G2/d}' $(BUILD_DIR)/black_box/$(DESIGN_NAME)/$(DESIGN_NAME).lef
+	sed -i -e '/OBS/,/END/{/LAYER J3/d}' $(BUILD_DIR)/black_box/$(DESIGN_NAME)/$(DESIGN_NAME).lef
+	sed -i -e '/OBS/,/END/{/LAYER A4/d}' $(BUILD_DIR)/black_box/$(DESIGN_NAME)/$(DESIGN_NAME).lef
+	sed -i -e '/OBS/,/END/{/LAYER CK/d}' $(BUILD_DIR)/black_box/$(DESIGN_NAME)/$(DESIGN_NAME).lef
+	sed -i -e '/OBS/,/END/{/LAYER U1/d}' $(BUILD_DIR)/black_box/$(DESIGN_NAME)/$(DESIGN_NAME).lef
+	sed -i -e '/OBS/,/END/{/LAYER U2/d}' $(BUILD_DIR)/black_box/$(DESIGN_NAME)/$(DESIGN_NAME).lef
+	sed -i -e '/OBS/,/END/{/LAYER U3/d}' $(BUILD_DIR)/black_box/$(DESIGN_NAME)/$(DESIGN_NAME).lef
+	sed -i -e '/OBS/,/END/{/LAYER KH/d}' $(BUILD_DIR)/black_box/$(DESIGN_NAME)/$(DESIGN_NAME).lef
+	sed -i -e '/OBS/,/END/{/LAYER N1/d}' $(BUILD_DIR)/black_box/$(DESIGN_NAME)/$(DESIGN_NAME).lef
+	sed -i -e '/OBS/,/END/{/LAYER HG/d}' $(BUILD_DIR)/black_box/$(DESIGN_NAME)/$(DESIGN_NAME).lef
+	sed -i -e '/OBS/,/END/{/LAYER T1/d}' $(BUILD_DIR)/black_box/$(DESIGN_NAME)/$(DESIGN_NAME).lef
 	cd $(BUILD_DIR)/black_box/$(DESIGN_NAME) && \
 		$(ICC2_LM_SHELL) -f $(BSG_DESIGNS_TARGET_DIR)/tcl/hard/gf_14/black_box/lef2ndm.tcl
 	touch $(BUILD_VPATH)/$@
