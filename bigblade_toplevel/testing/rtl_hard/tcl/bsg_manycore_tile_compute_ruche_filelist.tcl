85c8fc76163a7669ce08bfb445a3692b29c011a2
Max Ruttenberg <mrutt@cs.washington.edu>
2021-06-08 07:00:23 -0700

    [bigblade_toplevel] commit what we have

---
+++ bigblade_toplevel/testing/rtl_hard/tcl/bsg_manycore_tile_compute_ruche_filelist.tcl
@@ -0,0 +1,21 @@
+#------------------------------------------------------------
+# Do NOT arbitrarily change the order of files. Some module
+# and macro definitions may be needed by the subsequent files
+#------------------------------------------------------------
+
+set basejump_stl_dir       $::env(BASEJUMP_STL_DIR)
+set bsg_manycore_dir       $::env(BSG_MANYCORE_DIR)
+set bsg_designs_dir        $::env(BSG_DESIGNS_DIR)
+set bsg_designs_target_dir $::env(BSG_DESIGNS_TARGET_DIR)
+set bsg_packaging_dir      $::env(BSG_PACKAGING_DIR)
+
+set bsg_package       $::env(BSG_PACKAGE)
+set bsg_pinout        $::env(BSG_PINOUT)
+set bsg_padmapping    $::env(BSG_PADMAPPING)
+
+set bsg_out_dir       $::env(TESTING_BSG_OUT_DIR)
+
+set BSG_MANYCORE_TILE_SOURCE_FILES [join "
+    $bsg_designs_target_dir/../bigblade_manycore_tile/v/hard/gf_14/bsg_mem_1rw_sync_mask_write_byte.v
+    $bsg_designs_target_dir/../bigblade_manycore_tile/v/hard/gf_14/bsg_mem_1rw_sync.v
+"]
