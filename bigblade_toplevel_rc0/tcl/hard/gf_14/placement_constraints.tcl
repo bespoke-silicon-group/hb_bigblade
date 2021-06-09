eb195d56d37454574aa72417733bf4f4c53faf8b
Paul Gao <gaozihou@uw.edu>
2021-06-08 23:29:45 -0700

    toplevel fixes

--- bigblade_toplevel_rc0/tcl/hard/gf_14/placement_constraints.tcl
+++ bigblade_toplevel_rc0/tcl/hard/gf_14/placement_constraints.tcl
@@ -4,7 +4,7 @@ set BLOCK_HEIGHT [get_attribute $BLOCK height]
 set BLOCK_X      [expr ([die_width]  - $BLOCK_WIDTH)  / 2.0]
 set BLOCK_Y      [expr ([die_height] - $BLOCK_HEIGHT) / 2.0]
 
-set BLOCK_FIX_X 0.85
+set BLOCK_FIX_X 0
 set BLOCK_FIX_Y 0
 
 set_fixed_objects -unfix $BLOCK
