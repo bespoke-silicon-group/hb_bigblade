eed5ae9eac9a6d2e1685ea27daec026e5c76a59a
Dustin Richmond <dustinar@uw.edu>
2021-06-10 15:15:54 -0700

    Remove instance, replace with cell for bsg_chip

--- bigblade_toplevel/testing/post_synth/bsg_config.v
+++ bigblade_toplevel/testing/post_synth/bsg_config.v
@@ -1,7 +1,6 @@
 config bsg_config;
   design `BSG_DESIGNS_TESTING_LIBRARY_NAME.`BSG_TOP_SIM_MODULE;
   default liblist `BSG_DESIGNS_TESTING_LIBRARY_NAME `BSG_CHIP_LIBRARY_NAME work;
-  instance `BSG_CHIP_INSTANCE_PATH liblist `BSG_CHIP_LIBRARY_NAME;
 
   // each ip block with its library
   cell bsg_manycore_tile_compute_ruche liblist `BSG_MANYCORE_TILE_LIBRARY_NAME `BSG_CHIP_LIBRARY_NAME;
