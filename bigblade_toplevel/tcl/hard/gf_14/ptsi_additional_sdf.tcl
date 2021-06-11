d1d13c25d478c4bf9e283db972476959dda8ffb9
Dan Petrisko <petrisko@cs.washington.edu>
2021-06-11 11:11:41 -0700

    Reverting changes to toplevel

---
+++ bigblade_toplevel/tcl/hard/gf_14/ptsi_additional_sdf.tcl
@@ -0,0 +1,22 @@
+
+write_sdf -compress gzip -version 3.0 -context Verilog -level 2 ${RESULTS_DIR}/${DESIGN_NAME}_level_2.sdf
+
+write_sdf -compress gzip -version 3.0 -context Verilog -instance "block/io_link" ${RESULTS_DIR}/${DESIGN_NAME}_noc_io.sdf
+write_sdf -compress gzip -version 3.0 -context Verilog -instance "block/mem_link_0__link" ${RESULTS_DIR}/${DESIGN_NAME}_noc_mem_0.sdf
+write_sdf -compress gzip -version 3.0 -context Verilog -instance "block/mem_link_1__link" ${RESULTS_DIR}/${DESIGN_NAME}_noc_mem_1.sdf
+write_sdf -compress gzip -version 3.0 -context Verilog -instance "block/mem_link_2__link" ${RESULTS_DIR}/${DESIGN_NAME}_noc_mem_2.sdf
+write_sdf -compress gzip -version 3.0 -context Verilog -instance "block/mem_link_3__link" ${RESULTS_DIR}/${DESIGN_NAME}_noc_mem_3.sdf
+write_sdf -compress gzip -version 3.0 -context Verilog -instance "block/mem_link_4__link" ${RESULTS_DIR}/${DESIGN_NAME}_noc_mem_4.sdf
+write_sdf -compress gzip -version 3.0 -context Verilog -instance "block/mem_link_5__link" ${RESULTS_DIR}/${DESIGN_NAME}_noc_mem_5.sdf
+write_sdf -compress gzip -version 3.0 -context Verilog -instance "block/mem_link_6__link" ${RESULTS_DIR}/${DESIGN_NAME}_noc_mem_6.sdf
+write_sdf -compress gzip -version 3.0 -context Verilog -instance "block/mem_link_7__link" ${RESULTS_DIR}/${DESIGN_NAME}_noc_mem_7.sdf
+
+write_sdf -compress gzip -version 3.0 -context Verilog -instance "block/core_complex_core_0__clk_gen" ${RESULTS_DIR}/${DESIGN_NAME}_clk_gen_0.sdf
+write_sdf -compress gzip -version 3.0 -context Verilog -instance "block/core_complex_core_1__clk_gen" ${RESULTS_DIR}/${DESIGN_NAME}_clk_gen_1.sdf
+write_sdf -compress gzip -version 3.0 -context Verilog -instance "block/core_complex_core_2__clk_gen" ${RESULTS_DIR}/${DESIGN_NAME}_clk_gen_2.sdf
+write_sdf -compress gzip -version 3.0 -context Verilog -instance "block/core_complex_core_3__clk_gen" ${RESULTS_DIR}/${DESIGN_NAME}_clk_gen_3.sdf
+
+write_sdf -compress gzip -version 3.0 -context Verilog -instance "block/core_complex_core_0__podrow" ${RESULTS_DIR}/${DESIGN_NAME}_podrow_0.sdf
+write_sdf -compress gzip -version 3.0 -context Verilog -instance "block/core_complex_core_1__podrow" ${RESULTS_DIR}/${DESIGN_NAME}_podrow_1.sdf
+write_sdf -compress gzip -version 3.0 -context Verilog -instance "block/core_complex_core_2__podrow" ${RESULTS_DIR}/${DESIGN_NAME}_podrow_2.sdf
+write_sdf -compress gzip -version 3.0 -context Verilog -instance "block/core_complex_core_3__podrow" ${RESULTS_DIR}/${DESIGN_NAME}_podrow_3.sdf
--- bigblade_toplevel/testing/post_synth/bsg_config.v
+++ bigblade_toplevel/testing/post_synth/bsg_config.v
@@ -1,7 +1,6 @@
 config bsg_config;
   design `BSG_DESIGNS_TESTING_LIBRARY_NAME.`BSG_TOP_SIM_MODULE;
   default liblist `BSG_DESIGNS_TESTING_LIBRARY_NAME `BSG_CHIP_LIBRARY_NAME work;
-  instance `BSG_CHIP_INSTANCE_PATH liblist `BSG_CHIP_LIBRARY_NAME;
 
   // each ip block with its library
   cell bsg_manycore_tile_compute_ruche liblist `BSG_MANYCORE_TILE_LIBRARY_NAME `BSG_CHIP_LIBRARY_NAME;
@@ -31,10 +30,10 @@ config bsg_config;
   cell bsg_manycore_link_wh_to_sdr_se liblist `BSG_MANYCORE_LINK_SDR_LIBRARY_NAME `BSG_CHIP_LIBRARY_NAME;
   cell bsg_manycore_link_wh_to_sdr_sw liblist `BSG_MANYCORE_LINK_SDR_LIBRARY_NAME `BSG_CHIP_LIBRARY_NAME;
 
-  // bsg tie hi/lo
   cell bsg_tiehi liblist `BSG_TIEHILO_LIBRARY_NAME `BSG_CHIP_LIBRARY_NAME;
   cell bsg_tielo liblist `BSG_TIEHILO_LIBRARY_NAME `BSG_CHIP_LIBRARY_NAME;
   
   cell bsg_chip_block liblist `BIGBLADE_TOPLEVEL_BLOCK_LIBRARY_NAME `BSG_CHIP_LIBRARY_NAME;
+  cell bsg_chip liblist `BIGBLADE_TOPLEVEL_LIBRARY_NAME `BSG_CHIP_LIBRARY_NAME;
 
 endconfig
