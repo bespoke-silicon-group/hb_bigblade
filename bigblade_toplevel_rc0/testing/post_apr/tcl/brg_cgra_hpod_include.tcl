c47eec9d9aeaad3e8ec3db4c0aa1b4af13315285
Max Ruttenberg <mrutt@cs.washington.edu>
2021-06-13 21:30:48 -0700

    another fix

--- bigblade_toplevel_rc0/testing/post_apr/tcl/brg_cgra_hpod_include.tcl
+++ bigblade_toplevel_rc0/testing/post_apr/tcl/brg_cgra_hpod_include.tcl
@@ -13,7 +13,7 @@ set bsg_package       $::env(BSG_PACKAGE)
 set bsg_pinout        $::env(BSG_PINOUT)
 set bsg_padmapping    $::env(BSG_PADMAPPING)
 
-set BRG_CGRA_HPOD_INCLUDE_PATHS [ join "
+set BIGBLADE_BRG_CGRA_EXCEL_INCLUDE_PATHS [ join "
     $basejump_stl_dir/bsg_misc
     $basejump_stl_dir/hard/gf_14/bsg_mem
 "]
